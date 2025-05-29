88998cffc74da6c62e3556976dceff4b168aea9a
pramilaOM <pramilayadavomswami@gmail.com>
2025-05-29 14:51:59 -0400

    Time: 1 ms (99.66%), Space: 42.3 MB (63.78%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            int len = result.length();
+            if (len > 0 && Math.abs(result.charAt(len - 1) - c) == 32) {
+                result.deleteCharAt(len - 1);
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+}
