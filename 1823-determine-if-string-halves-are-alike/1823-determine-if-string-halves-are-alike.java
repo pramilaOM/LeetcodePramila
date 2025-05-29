bc486e34031ce6bec14ac06b24e7ffd5705500a8
pramilaOM <pramilayadavomswami@gmail.com>
2025-05-29 15:55:22 -0400

    Time: 4 ms (61.09%), Space: 41.7 MB (74.52%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -1,32 +1,22 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
         int n = s.length();
-        String s1 = s.substring(0, n / 2);
-        String s2 = s.substring(n / 2, n);
-        int countVowelS1 = 0;
-        int countVowelS2 = 0;
-        for (char c : s1.toCharArray()) {
-            if (isVowel(c)) {
-                countVowelS1 += 1;
-            }
+        int count = 0;
+        for (int i = 0; i < n / 2; i++) {
+            if (isVowel(s.charAt(i)))
+                count++;
+            if (isVowel(s.charAt(i + n / 2)))
+                count--;
         }
-        for (char c : s2.toCharArray()) {
-            if (isVowel(c)) {
-                countVowelS2 += 1;
-            }
-
-        }
-        return countVowelS1 == countVowelS2;
+        return count == 0;
 
     }
+
+    static Set<Character> allVowel = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
 
     public static boolean isVowel(char c) {
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
-                || c == 'U') {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
+        return allVowel.contains(c);
+
+    }
 }
