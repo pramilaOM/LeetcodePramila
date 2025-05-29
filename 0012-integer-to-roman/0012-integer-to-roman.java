8ca356a3d220182ffe6d104068dff5f210f40791
pramilaOM <pramilayadavomswami@gmail.com>
2025-05-29 02:46:03 -0400

    Time: 4 ms (42.73%), Space: 44.2 MB (92.51%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,18 +3,14 @@ class Solution {
         int[] val = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
         String[] sym = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
         String result = "";
-        for (int i = 0; i < 13; i++) {
-            if (num == 0) {
-                break;
+        int i = 0;
+        while(num > 0) {
+            while(num >= val[i]) {
+                num -= val[i];
+                result += sym[i];
             }
-            int times = num / val[i];
-            while (times > 0) {
-                result = result + sym[i];
-                times = times - 1;
-            }
-            num = num % val[i];
+            i++;
         }
-
         return result;
         
     }
