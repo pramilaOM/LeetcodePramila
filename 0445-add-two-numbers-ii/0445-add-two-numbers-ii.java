8dbea22f1ab36074e02c7e874d31dd2dae81fff9
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-01 00:46:07 -0400

    Time: 3 ms (42.97%), Space: 44.5 MB (58.22%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,41 +10,38 @@
  */
 class Solution {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        l1 = reverse(l1);
-        l2 = reverse(l2);
-        int sum = 0, carry = 0;
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
         ListNode ans = null;
 
-        while (l1 != null || l2 != null || carry > 0) {
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
+        while (!s1.isEmpty() || !s2.isEmpty() || carry > 0) {
+            int sum = carry;
+
+            if (!s1.isEmpty())
+                sum += s1.pop();
+            if (!s2.isEmpty())
+                sum += s2.pop();
 
             ListNode newNode = new ListNode(sum % 10);
             newNode.next = ans;
             ans = newNode;
 
             carry = sum / 10;
-            sum = carry;
         }
 
         return ans;
     }
 
-    public static ListNode reverse(ListNode head) {
-        // TODO Auto-generated method stub
-
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode last = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
-        return last;
-    }
 }
