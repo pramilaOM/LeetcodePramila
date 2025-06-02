1fdb114d786aac2e963d19a309eefe7c16188c35
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-02 18:43:49 -0400

    Time: 8 ms (11.72%), Space: 44.9 MB (32.39%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -2,12 +2,18 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
         int result = 0;
+        Map<String, BiFunction<Integer, Integer, Integer>> map = new HashMap<>();
+        map.put("+", (a, b) -> a + b);
+        map.put("-", (a, b) -> a - b);
+        map.put("*", (a, b) -> a * b);
+        map.put("/", (a, b) -> a / b);
+
         for (String token : tokens) {
             if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
                 int a = stack.pop();
                 int b = stack.pop();
 
-                result = operator(b, a, token);
+                result = map.get(token).apply(b, a);
                 stack.push(result);
 
             } else {
@@ -17,26 +23,5 @@ class Solution {
         }
         return stack.pop();
 
-    }
-
-    public static int operator(int b, int a, String token) {
-        // TODO Auto-generated method stub
-        if (token.equals("+")) {
-            return b + a;
-        }
-
-        if (token.equals("-")) {
-            return b - a;
-        }
-
-        if (token.equals("*")) {
-            return b * a;
-        }
-
-        if (token.equals("/")) {
-            return b / a;
-        }
-
-        return -1;
     }
 }
