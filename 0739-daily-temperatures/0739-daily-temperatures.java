c9795d93e3ec5b9af566b2df438f017b3e8c1f70
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-02 19:25:29 -0400

    Time: 78 ms (30.58%), Space: 60.6 MB (52.08%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        Stack<Integer> stack = new Stack<>();
+        int size = temperatures.length;
+        int[] result = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            if (stack.empty()) {
+                result[i] = 0;
+            } else {
+                result[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+
+        return result;
+
+    }
+}
