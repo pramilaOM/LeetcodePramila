2db667afc16310c09ac504a2e128f70c44efe72a
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-03 03:39:13 -0400

    Time: 2 ms (98.51%), Space: 57.2 MB (53.73%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
@@ -4,7 +4,7 @@ class Solution {
     public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
         int n = status.length;
         boolean[] hasKey = new boolean[n];
-        boolean[] used = new boolean[n];  // to avoid processing same box again
+        boolean[] used = new boolean[n]; 
         boolean[] boxAvailable = new boolean[n];
 
         Queue<Integer> queue = new LinkedList<>();
@@ -19,17 +19,12 @@ class Solution {
             progress = false;
             for (int i = 0; i < n; i++) {
                 if (!used[i] && boxAvailable[i] && (status[i] == 1 || hasKey[i])) {
-                    // Process this box
                     totalCandies += candies[i];
                     used[i] = true;
                     progress = true;
-
-                    // Gain new keys
                     for (int key : keys[i]) {
                         hasKey[key] = true;
                     }
-
-                    // Discover contained boxes
                     for (int box : containedBoxes[i]) {
                         boxAvailable[box] = true;
                     }
