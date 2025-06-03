f22c5d295c7a25cd39c0524227a50f2359cd401b
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-03 03:34:05 -0400

    Time: 20 ms (20.9%), Space: 57.5 MB (43.78%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
@@ -1,55 +1,42 @@
 class Solution {
-
-    public int dfs(int box, int[] status, int[] candies, int[][] keys, int[][] containedBoxes,
-                   Set<Integer> visited, Set<Integer> foundBoxes) {
-
-        if (visited.contains(box)) {
-            return 0;
-        }
-
-        if (status[box] == 0) {
-            foundBoxes.add(box);
-            return 0;
-        }
-
-        visited.add(box);
-        int candiesCollected = candies[box];
-
-        // First process keys
-        for (int boxKey : keys[box]) {
-            status[boxKey] = 1;
-            if (foundBoxes.contains(boxKey)) {
-                candiesCollected += dfs(boxKey, status, candies, keys, containedBoxes, visited, foundBoxes);
-            }
-        }
-
-        // Then process contained boxes
-        for (int innerBox : containedBoxes[box]) {
-            candiesCollected += dfs(innerBox, status, candies, keys, containedBoxes, visited, foundBoxes);
-        }
-
-        return candiesCollected;
-    }
-
-    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
-
+    public int maxCandies(int[] status, int[] candies,
+                          int[][] keys, int[][] containedBoxes,
+                          int[] initialBoxes) {
+        
         int candiesCollected = 0;
         Set<Integer> visited = new HashSet<>();
         Set<Integer> foundBoxes = new HashSet<>();
+        Queue<Integer> que = new LinkedList<>();
 
-        Queue<Integer> queue = new LinkedList<>();
         for (int box : initialBoxes) {
-            queue.add(box);
+            foundBoxes.add(box);
+            if (status[box] == 1) {
+                que.offer(box);
+                visited.add(box);
+                candiesCollected += candies[box];
+            }
         }
 
-        while (!queue.isEmpty()) {
-            int box = queue.poll();
-            int before = visited.size();
-            candiesCollected += dfs(box, status, candies, keys, containedBoxes, visited, foundBoxes);
-            // Revisit boxes in foundBoxes in case new keys unlocked them
-            if (visited.size() > before) {
-                for (int b : new HashSet<>(foundBoxes)) {
-                    queue.offer(b);
+        while (!que.isEmpty()) {
+            int box = que.poll();
+
+            // Process contained boxes
+            for (int innerBox : containedBoxes[box]) {
+                foundBoxes.add(innerBox);
+                if (status[innerBox] == 1 && !visited.contains(innerBox)) {
+                    que.offer(innerBox);
+                    visited.add(innerBox);
+                    candiesCollected += candies[innerBox];
+                }
+            }
+
+            // Process keys
+            for (int boxKey : keys[box]) {
+                status[boxKey] = 1; // mark as openable
+                if (foundBoxes.contains(boxKey) && !visited.contains(boxKey)) {
+                    que.offer(boxKey);
+                    visited.add(boxKey);
+                    candiesCollected += candies[boxKey];
                 }
             }
         }
