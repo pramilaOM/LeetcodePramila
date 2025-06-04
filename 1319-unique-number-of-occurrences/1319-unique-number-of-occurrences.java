4e749a0efd87c2485d73f864a66b8ef59930a324
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-03 22:21:07 -0400

    Time: 7 ms (6.15%), Space: 43 MB (5.84%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
+
         int[] data = new int[2001];
 
         for (int x : arr) {
@@ -8,9 +9,16 @@ class Solution {
         Arrays.sort(data);
 
         for (int i = 1; i < 2001; i++) {
-            if (data[i] != 0 && data[i] == data[i - 1]) {
+            if (data[i] == 0)
+                continue;
+
+            int idx = Math.abs(data[i]);
+
+            if (data[idx] < 0) {
                 return false;
             }
+
+            data[idx] = -1;
         }
 
         return true;
