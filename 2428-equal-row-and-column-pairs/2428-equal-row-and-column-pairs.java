3f353802cbca9d994d8bb33f656e2295cf3b41bd
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-04 14:27:11 -0400

    Time: 17 ms (93.05%), Space: 50.6 MB (55.41%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -3,22 +3,24 @@ class Solution {
         int n = grid.length;
         int count = 0;
 
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                boolean isEqual = true;
+        Map<List<Integer>, Integer> rowMap = new HashMap<>();
 
-                for (int i = 0; i < n; i++) {
-                    if (grid[r][i] != grid[i][c]) {
-                        isEqual = false;
-                        break;
-                    }
-                }
-                if (isEqual) {
-                    count++;
-                }
+        for (int r = 0; r < n; r++) {
+            List<Integer> row = new ArrayList<>();
+            for (int c = 0; c < n; c++) {
+                row.add(grid[r][c]);
             }
+            rowMap.put(row, rowMap.getOrDefault(row, 0) + 1);
         }
 
+        for (int c = 0; c < n; c++) {
+            List<Integer> col = new ArrayList<>();
+            for (int r = 0; r < n; r++) {
+                col.add(grid[r][c]);
+            }
+            count += rowMap.getOrDefault(col, 0);
+
+        }
         return count;
     }
 }
