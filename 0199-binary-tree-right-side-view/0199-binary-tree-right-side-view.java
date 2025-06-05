99c1b25e60803107638b8a207e4a87eeaaede885
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-04 20:55:42 -0400

    Time: 1 ms (71.78%), Space: 42.4 MB (27.99%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -17,20 +17,29 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
 
         List<Integer> result = new ArrayList<>();
-        dfs(root, 1, result);
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            TreeNode rightNode = null;
+
+            for (int i = 0; i < levelSize; i++) {
+                rightNode = queue.poll();
+
+                if (rightNode.left != null) {
+                    queue.offer(rightNode.left);
+                }
+                if (rightNode.right != null) {
+                    queue.offer(rightNode.right);
+                }
+            }
+            result.add(rightNode.val);
+        }
         return result;
 
-    }
-
-    public void dfs(TreeNode node, int level, List<Integer> result) {
-        if (node == null)
-            return;
-
-        if (result.size() < level) {
-            result.add(node.val);
-        }
-
-        dfs(node.right, level + 1, result);
-        dfs(node.left, level + 1, result);
     }
 }
