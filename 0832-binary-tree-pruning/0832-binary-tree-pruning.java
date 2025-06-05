868d660dbc5408e2676997d91ee8e01560de51a6
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-05 16:30:48 -0400

    Time: 0 ms (100%), Space: 41.5 MB (38.17%) - LeetHub

--- 0832-binary-tree-pruning/0832-binary-tree-pruning.java
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.java
@@ -15,16 +15,6 @@
  */
 class Solution {
 
-    public boolean checkOne(TreeNode root) {
-        if (root == null) {
-            return false;
-        }
-        if (root.val == 1) {
-            return true;
-        }
-        return checkOne(root.left) || checkOne(root.right);
-    }
-
     public TreeNode pruneTree(TreeNode root) {
 
         if (root == null) {
@@ -33,11 +23,6 @@ class Solution {
 
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
-
-        if (!checkOne(root.left))
-            root.left = null;
-        if (!checkOne(root.right))
-            root.right = null;
 
         if (root.left == null && root.right == null && root.val == 0) {
             return null;
