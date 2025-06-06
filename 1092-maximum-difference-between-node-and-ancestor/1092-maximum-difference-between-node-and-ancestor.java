34133e7465025fafc2b878c041167bbc1ed7c74f
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-06 01:09:47 -0400

    Time: 118 ms (6.7%), Space: 42.1 MB (55.1%) - LeetHub

---
+++ 1092-maximum-difference-between-node-and-ancestor/1092-maximum-difference-between-node-and-ancestor.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDiff;
+
+    void findMaxUtil(TreeNode root, TreeNode child) {
+        if (root == null || child == null) {
+            return;
+        }
+
+        maxDiff = Math.max(maxDiff, Math.abs(root.val - child.val));
+
+        findMaxUtil(root, child.left);
+        findMaxUtil(root, child.right);
+    }
+
+    void findMaxDiff(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return;
+        }
+
+        findMaxUtil(root, root.left);
+        findMaxUtil(root, root.right);
+
+        findMaxDiff(root.left);
+        findMaxDiff(root.right);
+    }
+
+    public int maxAncestorDiff(TreeNode root) {
+        maxDiff = Integer.MIN_VALUE;
+        findMaxDiff(root);
+        return maxDiff;
+
+    }
+}
