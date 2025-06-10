338aee454e574c840ad6c5d7569319414b5c4399
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-10 15:45:43 -0400

    Time: 1 ms (99.17%), Space: 41.8 MB (34.55%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxScore(String s) {
-
-        int oneCount = 0;
-        int zeroCount = 0;
-        int maxDiff = Integer.MIN_VALUE;
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == '0') {
-                zeroCount++;
+        int totalOne = 0;
+        for (int i : s.toCharArray()) {
+            if (i == '1') {
+                totalOne++;
             }
-            oneCount = 0;
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(j) == '1') {
-                    oneCount++;
-                }
-            }
-            maxDiff = Math.max(maxDiff, zeroCount + oneCount);
         }
-    return maxDiff;
+        int left = 0;
+        int right = totalOne;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < s.length()-1; i++) {
+            if (s.charAt(i) == '0') {
+                left++;
+            } else {
+                right--;
+            }
+            max = Math.max(max, left+right);
+        }
+        return max;
     }
 }
