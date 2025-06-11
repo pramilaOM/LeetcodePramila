31327c5f77db1206f670ae6f1b0b9e042088960a
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-11 02:39:44 -0400

    Time: 2 ms (68.23%), Space: 42.2 MB (6.35%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
@@ -1,18 +1,23 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
         int sum = 0;
-        int n = arr.length;
-        int [] prefix = new int[n+1];
-        for(int i = 1; i <=n ; i++){
-            prefix[i] = arr[i-1] + prefix[i-1];
+
+        int[] prefiXSum = new int[arr.length];
+        prefiXSum[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefiXSum[i] = prefiXSum[i - 1] + arr[i];
         }
         for (int i = 0; i < arr.length; i++) {
-            int arrSum = 0;
-            for (int j = i; j < arr.length; j+=2) {
-                arrSum += (prefix[j+1] - prefix[i]);
+            for (int j = i; j < arr.length; j += 2) {
+                if (i - 1 >= 0) {
+                    sum = prefiXSum[j] + sum - prefiXSum[i - 1];
+                } else {
+                    sum = prefiXSum[j] + sum;
+                }
+
             }
-            sum += arrSum;
         }
         return sum;
+
     }
 }
