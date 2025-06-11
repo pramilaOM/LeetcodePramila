2d0114cf921c461ad86b1cc65431338feab0a7d0
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-11 03:01:04 -0400

    Time: 2 ms (68.23%), Space: 41.4 MB (47.24%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
@@ -10,7 +10,7 @@ class Solution {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j += 2) {
                 if (i - 1 >= 0) {
-                    sum = prefiXSum[j] + sum - prefiXSum[i - 1];
+                    sum += prefiXSum[j] - prefiXSum[i - 1];
                 } else {
                     sum = prefiXSum[j] + sum;
                 }
