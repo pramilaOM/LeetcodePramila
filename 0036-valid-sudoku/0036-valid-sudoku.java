ee61ce1b2b7c11a8888140a0fa9ea3b7b1e7d7d1
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-16 16:44:41 -0400

    Time: 14 ms (15.89%), Space: 44.9 MB (32.73%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,57 +1,28 @@
-import java.util.HashSet;
-
 class Solution {
-    private boolean isValidSubBox(char[][] board, int sr, int er, int sc, int ec) {
-        HashSet<Character> set = new HashSet<>();
-        for (int row = sr; row <= er; row++) {
-            for (int col = sc; col <= ec; col++) {
-                char ch = board[row][col];
-                if (ch == '.')
-                    continue;
-                if (set.contains(ch))
-                    return false;
-                set.add(ch);
-            }
-        }
-        return true;
-    }
-
     public boolean isValidSudoku(char[][] board) {
-        // Validate rows
-        for (int row = 0; row < 9; row++) {
-            HashSet<Character> set = new HashSet<>();
-            for (int col = 0; col < 9; col++) {
-                char ch = board[row][col];
-                if (ch == '.')
-                    continue;
-                if (set.contains(ch))
-                    return false;
-                set.add(ch);
-            }
-        }
+        HashSet<String> seen = new HashSet<>();
 
-        // Validate columns
-        for (int col = 0; col < 9; col++) {
-            HashSet<Character> set = new HashSet<>();
-            for (int row = 0; row < 9; row++) {
-                char ch = board[row][col];
-                if (ch == '.')
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num == '.')
                     continue;
-                if (set.contains(ch))
-                    return false;
-                set.add(ch);
-            }
-        }
 
-        // Validate 3x3 sub-boxes
-        for (int sr = 0; sr < 9; sr += 3) {
-            for (int sc = 0; sc < 9; sc += 3) {
-                if (!isValidSubBox(board, sr, sr + 2, sc, sc + 2)) {
+                String rowKey = num + "_row_" + i;
+                String colKey = num + "_col_" + j;
+                String boxKey = num + "_row_" + (i / 3) + "_" + (j / 3);
+
+                if (seen.contains(rowKey) || seen.contains(colKey) || seen.contains(boxKey)) {
                     return false;
                 }
+
+                seen.add(rowKey);
+                seen.add(colKey);
+                seen.add(boxKey);
+
             }
         }
-
         return true;
+
     }
 }
