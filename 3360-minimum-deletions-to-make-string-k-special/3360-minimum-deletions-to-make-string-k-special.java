1aaf69bdc34c062f70267377284b98a471fea94d
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-21 16:52:51 -0400

    Time: 6 ms (50.99%), Space: 45.6 MB (53.64%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumDeletions(String word, int k) {
+
+        int[] freq = new int[26];
+
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int result = word.length();
+        int cumDelete = 0;
+
+        for (int i = 0; i < 26; i++) {
+            int del = cumDelete;
+            for (int j = 25; j > i; j--) {
+                if (freq[j] - freq[i] <= k) {
+                    break;
+                }
+                del += freq[j] - freq[i] - k;
+            }
+            result = Math.min(result, del);
+            cumDelete += freq[i];
+        }
+        return result;
+    }
+}
