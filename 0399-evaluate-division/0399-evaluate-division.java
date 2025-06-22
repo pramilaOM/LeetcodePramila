61db6fbb77fdcbb973a6c32c053d99f59ef21bbe
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-21 23:29:10 -0400

    Time: 1 ms (98.97%), Space: 42.6 MB (14.64%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -0,0 +1,54 @@
+class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+
+        for (int i = 0; i < equations.size(); i++) {
+            String u = equations.get(i).get(0);
+            String v = equations.get(i).get(1);
+            double val = values[i];
+
+            graph.putIfAbsent(u, new HashMap<>());
+            graph.putIfAbsent(v, new HashMap<>());
+
+            graph.get(u).put(v, val);
+            graph.get(v).put(u, 1.0 / val);
+        }
+
+        double[] result = new double[queries.size()];
+
+        for (int i = 0; i < queries.size(); i++) {
+            String start = queries.get(i).get(0);
+            String end = queries.get(i).get(1);
+
+            if (!graph.containsKey(start) || !graph.containsKey(end)) {
+                result[i] = -1.0;
+            } else if (start.equals(end)) {
+                result[i] = 1.0;
+            } else {
+                Set<String> visited = new HashSet<>();
+                result[i] = dfs(graph, start, end, 1.0, visited);
+            }
+        }
+        return result;
+    }
+
+    double dfs(Map<String, Map<String, Double>> graph, String current, String target, double value,
+            Set<String> visited) {
+        if (current.equals(target)) {
+            return value;
+        }
+        visited.add(current);
+
+        for (Map.Entry<String, Double> neighbor : graph.get(current).entrySet()) {
+            if (!visited.contains(neighbor.getKey())) {
+                double result = dfs(graph, neighbor.getKey(), target, value * neighbor.getValue(), visited);
+                if (result != -1.0) {
+                    return result;
+                }
+            }
+        }
+        return -1.0;
+
+    }
+}
