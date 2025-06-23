8589fc6ed1326a663c88b975b6f33f534422992d
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-23 16:57:10 -0400

    Time: 27 ms (77.95%), Space: 58.2 MB (54.9%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,14 +1,48 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+    void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
 
-        for (int n : nums) {
-            minheap.add(n);
-            if (minheap.size() > k) {
-                minheap.remove();
+    int partition(int[] nums, int L, int R) {
+        int P = nums[L];
+        int i = L + 1;
+        int j = R;
+        while (i <= j) {
+            if (nums[i] < P && nums[j] > P) {
+                swap(nums, i, j);
+                i++;
+                j--;
+            }
+            if (nums[i] >= P) {
+                i++;
+            }
+            if (nums[j] <= P) {
+                j--;
+            }
+
+        }
+        swap(nums, L, j);
+        return j;
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        int L = 0;
+        int R = n - 1;
+        int pivot_idx = 0;
+
+        while (true) {
+            pivot_idx = partition(nums, L, R);
+            if (pivot_idx == k - 1) {
+                break;
+            } else if (pivot_idx > k - 1) {
+                R = pivot_idx - 1;
+            } else {
+                L = pivot_idx + 1;
             }
         }
-        return minheap.peek();
-
+        return nums[pivot_idx];
     }
 }
