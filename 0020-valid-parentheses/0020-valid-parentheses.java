c20beb2948c0239e3e09fe2669a0ab31b31a4d80
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-26 02:15:21 -0400

    Time: 2 ms (96.81%), Space: 42.2 MB (12.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,17 +2,22 @@ class Solution {
     public boolean isValid(String s) {
 
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c =='{' || c =='[' || c =='('){
+        for (char c : s.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
                 stack.push(c);
-            }else{
-                if(stack.isEmpty()){
+            } else {
+                if (stack.isEmpty()) {
                     return false;
                 }
-                stack.pop();
+                char top = stack.pop();
 
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return false;
+                }
             }
+
         }
+
         return stack.isEmpty();
     }
 }
