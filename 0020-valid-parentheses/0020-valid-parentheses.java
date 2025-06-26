c280b90be510fdf3d9518882488b553b8b209c72
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-26 01:53:11 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
-        //brute force approach
-        //Repeatedly remove valid bracket pairs like "()", "[]", and "{}" from the string.
-        //If, after all possible removals, the string becomes empty → it is valid.
-        //If any characters remain → it is invalid.
 
-        //contains Checks if the string s contains the substring "()".
-        //replace Replaces all occurrences of "()" with "" (empty string), effectively removing them.
-        //isEmpty Checks if the string s has length 0 (i.e., it's empty).
-        while(s.contains("()") || s.contains("[]") || s.contains("{}")){
-            s= s.replace("()","");
-            s= s.replace("[]","");
-            s= s.replace("{}","");
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c =='{' || c =='[' || c =='('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+
+            }
         }
-        return s.isEmpty();
-        
+        return stack.isEmpty();
     }
 }
