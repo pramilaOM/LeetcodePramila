9c3e48fbb403486e38e1a147c59f4929b76dbfa1
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-26 02:16:33 -0400

    Time: 2 ms (96.81%), Space: 41.7 MB (81.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
-
+//Use a stack to track opening brackets.
+// For every character in the string:
+// If it's an opening bracket ((, [, {), push it onto the stack.
+// If it's a closing bracket (), ], }), check:
+// If the stack is empty, it's invalid (no matching open).
+// Pop from stack and verify that it matches the closing bracket.
+// At the end, the stack should be empty if all brackets were valid and matched.
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '{' || c == '[' || c == '(') {
