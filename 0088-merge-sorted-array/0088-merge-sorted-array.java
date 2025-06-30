4985022e39150bd080a83479cb90c09d3323491c
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-30 18:22:48 -0400

    Time: 9 ms (2.54%), Space: 42.6 MB (15.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,13 +1,34 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //BF
-        int element = 0;
-        for (int i = m; i < nums1.length; i++) {
-            nums1[i] = nums2[element];
-            element++;
+
+        //TC compare and write p1,p2 or p backword at monst m(nums1)+n(nums2) so O(m+n)
+        //SC all operation in place in nums1 O(1)
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;// fill from the end of nums1
+
+         
+        while (p1 >= 0 && p2 >= 0) {
+            //largest will be moved to end
+            if (nums1[p1] < nums2[p2]) {
+                nums1[p] = nums2[p2];
+                p2--;
+            } else {
+                nums1[p] = nums1[p1];
+                p1--;
+            }
+            p--;
+
         }
-
-        Arrays.sort(nums1);
-
+        //copy nums2 of remaining element
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+        for (int n1 : nums1) {
+            System.out.print(n1);
+        }
+        
     }
 }
