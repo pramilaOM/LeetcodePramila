b9a6c7023ee6fe949eccf0bd9919188edd91e693
pramilaOM <pramilayadavomswami@gmail.com>
2025-06-30 18:23:21 -0400

    Time: 16 ms (2.54%), Space: 42.7 MB (8.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,34 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        //TC compare and write p1,p2 or p backword at monst m(nums1)+n(nums2) so O(m+n)
-        //SC all operation in place in nums1 O(1)
-        int p1 = m - 1;
-        int p2 = n - 1;
-        int p = m + n - 1;// fill from the end of nums1
-
-         
-        while (p1 >= 0 && p2 >= 0) {
-            //largest will be moved to end
-            if (nums1[p1] < nums2[p2]) {
-                nums1[p] = nums2[p2];
-                p2--;
-            } else {
-                nums1[p] = nums1[p1];
-                p1--;
-            }
-            p--;
-
+        //TC O(m+n)log(m+n) 
+        //SC Sorting inplace no extra space hence O(1)
+        // add num2 in num1 TC O(n)
+        int index = 0;
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[index];
+            index++;
         }
-        //copy nums2 of remaining element
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2];
-            p2--;
-            p--;
-        }
+        //num1 has m+n element
+        // sort the element TC O(m+n)log(m+n)
+        Arrays.sort(nums1);// SC O(1)
         for (int n1 : nums1) {
-            System.out.print(n1);
+            System.out.print(n1 + " ");
         }
-        
     }
 }
