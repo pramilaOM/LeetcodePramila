81f833827fe43f611d4535ce8ee5877a018e06de
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-01 03:17:46 -0400

    Time: 81 ms (5.74%), Space: 47.4 MB (6.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        //Two pointers approach optimistic 
+        //TC O(N)
+        //SC O(1)
+
         int write = 1;
-        for (int read = 1; read < nums.length; read++) {
-            if (nums[read] != nums[read - 1]) {
+        for(int read = 1;read<nums.length;read++){
+            if(nums[read] != nums[read-1]){
                 nums[write] = nums[read];
                 write++;
             }
         }
-
+        for(int n : nums){
+            System.out.print(n+" ");
+        }
         return write;
     }
 }
