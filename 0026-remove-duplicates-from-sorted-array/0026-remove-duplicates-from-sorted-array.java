9bfad197990589b45489d288616c3658a313cb40
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-01 03:17:26 -0400

    Time: 1 ms (59.98%), Space: 45.5 MB (6.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,30 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        //BF
-        // Time Complexity O(n)
-        // Space Complexity O(n)
-        // iterate through nums, adding unique values to a temporary array arr.
-        int[] arr = new int[nums.length];
-        int k = 0;
-        for (int num : nums) {
-            if (k == 0) {
-                arr[k] = num;
-                k++;
-            } else {
-                if (num != arr[k - 1]) {
-                    arr[k] = num;
-                    k++;
-                }
+        int write = 1;
+        for (int read = 1; read < nums.length; read++) {
+            if (nums[read] != nums[read - 1]) {
+                nums[write] = nums[read];
+                write++;
             }
         }
-        // Copy back the unique values (arr[0 to k-1]) into the start of nums
-        for (int i = 0; i < k; i++) {
-            nums[i] = arr[i];
-        }
-        for (int n : nums) {
-            System.out.print(n + " ");
-        }
-        return k;
-        
+
+        return write;
     }
 }
