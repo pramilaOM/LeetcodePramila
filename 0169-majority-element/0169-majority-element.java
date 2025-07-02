caff1b13b867b1b048a49aa2c048267efc1448c7
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-02 02:24:45 -0400

    Time: 12 ms (36.06%), Space: 49.7 MB (80.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         int n = nums.length/2;
-        int ans = nums[0];
-        Arrays.sort(nums);
-        int count = 1;
-        for(int i =1;i<nums.length;i++){
-            if(nums[i] ==nums[i-1]){
-                count++;
-            }else{
-                count = 1;
+        //bf
+        // TC O(n) for building frequency map + O(n) for final scan → O(n) total
+        // SC O(n) for storing counts in a hash map or frequency structure
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int n : nums) {
+            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        }
+
+       for(Map.Entry<Integer,Integer> entry : freqMap.entrySet()){
+            if(entry.getValue() > nums.length/2){
+                return entry.getKey();
             }
 
-            if(count > n){
-                 ans = nums[i];
-            }
-        }
-       return ans;
-        
+       } 
+
+       return -1; 
+
     }
 }
