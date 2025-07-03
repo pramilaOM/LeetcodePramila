a18c494608b5956510fd1593637a373657b2885e
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-03 18:58:43 -0400

    Time: 2 ms (83.11%), Space: 45.9 MB (31.89%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //top down
         int len = nums.length;
-        boolean[] top = new boolean[len];
-        top[0]= true;
-        for(int i = 1;i<len;i++){
-            for(int j = i-1;j>=0;j--){
-                if(top[j] == true && j+nums[j] >= i){
-                    top[i] = true;
-                    break;
-                }
+        int maxReachable = 0;
+        for (int i = 0; i < len; i++) {
+            if (i > maxReachable) {
+                return false;
             }
+            maxReachable = Math.max(maxReachable, nums[i] + i);
+
         }
-        return top[len-1];
-        
+        return true;
+
     }
 }
