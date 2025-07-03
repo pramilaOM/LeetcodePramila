af1c709923b727ed49e90aaf5850250853f6cb6e
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-03 17:26:07 -0400

    Time: 358 ms (6.09%), Space: 45.3 MB (98.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,32 @@
 class Solution {
+    int[] memo = new int[10001];
     public boolean canJump(int[] nums) {
+        Arrays.fill(memo,-1);//-1 unvisited , 1 visited 
+        int len = nums.length;
+        return solve(nums, len, 0);
 
-        int lastPosition = nums.length-1;
-        for(int i = nums.length-2;i>=0;i--){
-            if(i+nums[i] >= lastPosition){
-                lastPosition=i;
+    }
+
+    private boolean solve(int[] nums, int len, int index) {
+
+        if (index == len - 1) {
+            return true;
+        }
+        if (nums[index] == 0) {
+            return false;
+        }
+
+        if(memo[index] != -1){
+            return memo[index] == 1;
+        }
+        for (int i = 1; i <= nums[index]; i++) {
+            if (solve(nums, len, index + i) == true) {
+                memo[index] = 1;
+                return true;
             }
         }
-        return lastPosition ==0;
+        memo[index] = 0;
+        return false;
     }
+
 }
