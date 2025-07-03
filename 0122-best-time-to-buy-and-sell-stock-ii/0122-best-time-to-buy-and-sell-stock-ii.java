1fb31374074091370b924babb49d303875eb6f45
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-02 23:58:17 -0400

    Time: 1 ms (79.01%), Space: 45.9 MB (47.5%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxPrice=0;
-        for(int i =0;i<prices.length-1;i++){
-            if(prices[i] < prices[i+1]){
-                maxPrice+= prices[i+1]-prices[i];
+        //Need to learn DP approach
+        //https://www.youtube.com/watch?v=Q7v239y-Tik
+        //TC O(n) Single pass over array
+        //SC O(1) No extra memory used
+        // greedy optimized solution
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += (prices[i] - prices[i - 1]);
             }
         }
-        return maxPrice;
+        return profit;
     }
 }
