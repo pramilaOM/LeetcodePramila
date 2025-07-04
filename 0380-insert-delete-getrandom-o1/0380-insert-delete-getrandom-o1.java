625d4be81fafbce42ac7cd619ebf3fd59b8fb03d
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-03 23:30:36 -0400

    Time: 223 ms (5.02%), Space: 89.1 MB (81.64%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,45 +1,36 @@
 class RandomizedSet {
-    private ArrayList<Integer> list;
-    private HashMap<Integer, Integer> map;
-    private Random rand;
+    List<Integer> list;
 
     public RandomizedSet() {
+        //initialise the object
         list = new ArrayList<>();
-        map = new HashMap<>();
-        rand = new Random();
-
     }
 
     public boolean insert(int val) {
-        if (map.containsKey(val)) {
+        if (!list.contains(val)) {
+            list.add(val);
+            return true;
+        } else {
             return false;
         }
-        list.add(val);
-        map.put(val, list.size() - 1);
-        return true;
 
     }
 
     public boolean remove(int val) {
-        if (!map.containsKey(val)) {
+        if (list.contains(val)) {
+            list.remove(Integer.valueOf(val));
+            return true;
+        } else {
             return false;
         }
-        int index = map.get(val);
-        int last = list.get(list.size() - 1);
-
-        list.set(index, last);
-        map.put(last, index);
-
-        list.remove(list.size() - 1);
-        map.remove(val);
-
-        return true;
 
     }
 
     public int getRandom() {
-        int random = rand.nextInt(list.size());
-        return list.get(random);
+        Random ran = new Random();
+        int index = ran.nextInt(list.size());
+        return list.get(index);
+
     }
 }
 
