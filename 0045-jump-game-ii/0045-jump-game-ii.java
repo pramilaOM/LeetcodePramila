ec77b0722b548099824998f285266203cee22ccd
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-03 20:24:53 -0400

    Time: 119 ms (8.34%), Space: 45.6 MB (24.86%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,33 @@
 class Solution {
+    int[] memo = new int[10001];
     public int jump(int[] nums) {
-        int jump = 0,end=0,farthest = 0;
-        for(int i =0;i<nums.length-1;i++){
-
-            farthest = Math.max(farthest, i+nums[i]);
-            if(i == end){
-                end = farthest;
-                jump++;
-
+         // BF
+        // Time Complexity: O(nⁿ) in the worst case (since it tries all jump paths
+        // recursively)
+        // Space Complexity: O(n) due to recursion stack depth
+        Arrays.fill(memo,-1);
+        return solve(nums, nums.length, 0);
+    }
+     private  int solve(int[] nums, int length, int i) {
+        // base case last index
+        if (i == length - 1) {
+            return 0;
+        }
+        if(memo[i] != -1){
+            return memo[i];
+        }
+        int minJump = Integer.MAX_VALUE;
+        // From index i, try every position from i+1 to i+nums[i]
+        for (int next = i + 1; next <= i + nums[i] && next < length; next++) {
+            // Recursively find the minimum jumps needed from each of those positions
+            int subroute = solve(nums, length, next);
+            if (subroute != Integer.MAX_VALUE) {
+                // Add 1 for the current jump
+                minJump = Math.min(1 + subroute, minJump);
             }
         }
-        return jump;
-        
+        memo[i]= minJump;
+        return minJump;
     }
+
 }
