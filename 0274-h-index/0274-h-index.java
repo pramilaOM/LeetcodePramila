d824b5abf1e50e8e5dfe29cb4d490f52d4dcc844
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-03 21:57:07 -0400

    Time: 17 ms (5.05%), Space: 41.8 MB (41.61%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,32 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int len = citations.length;
+         // bf Time Complexity: O(n²)
+        // For each of n possible h values, we scan the array (another n) to count valid
+        // papers.
+
+        // Space Complexity: O(1)
+        // Only a few variables used for tracking.
+
+        // Let n be the number of papers (length of the citations array).
+        int n = citations.length;
         int max = 0;
-        for (int i = 1; i <= len; i++) {
+        // h loops from 1 to n
+        for (int i = 1; i <= n; i++) {//IMP
+            // For each h, you count how many values in citations[] are ≥ h
             int count = 0;
-            for (int j = 0; j < len; j++) {
+            for (int j = 0; j < n; j++) {
                 if (citations[j] >= i) {
                     count++;
                 }
+
             }
+            // If count ≥ h, it's a valid H-index candidate
             if (count >= i) {
                 max = i;
             }
         }
+
         return max;
+        
     }
 }
