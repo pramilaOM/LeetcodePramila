05924965522aa809f52ce1945ca233e076f2ac53
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-11 03:14:54 -0400

    Time: 0 ms (100%), Space: 40.1 MB (92.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,25 @@
 class Solution {
 
+int[] t = new int[46];
     int solve(int step, int last) {
 
         if (step == last) {
-            return 1;
+            t[last] =1;
+            return t[last];
         } else if (step > last) {
             return 0;
         }
 
-        return solve(step + 1, last) + solve(step + 2, last);
+        if(t[step] !=-1){
+            return t[step];
+        }
 
+        t[step]= solve(step + 1, last) + solve(step + 2, last);
+        return t[step];
     }
 
     public int climbStairs(int n) {
-
+        Arrays.fill(t,-1);
         return solve(0, n);
 
     }
