bf244cc91a6d383737d17f4c121c6887a5b9745d
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-11 19:04:37 -0400

    Time: 13 ms (42.21%), Space: 45 MB (35.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         //myself
-        String withoutws = s.replaceAll("[^a-zA-Z0-9\\\\s]", "").toLowerCase();
-        withoutws = withoutws.replaceAll("\\\\", "");
+        String withoutws = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         int start = 0;
         int end = withoutws.length()  - 1;
         while (start < end) {
