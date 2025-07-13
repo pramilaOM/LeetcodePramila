9e0b405422d63162f83a98a23b8ce4a3434856a6
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-13 15:05:54 -0400

    Time: 137 ms (18.15%), Space: 44.4 MB (22.95%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        // Precompute all square numbers <= n
+        List<Integer> squares = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            squares.add(i * i);
+        }
+
+        // Bottom-up DP
+        for (int i = 1; i <= n; i++) {
+            for (int square : squares) {
+                if (i < square) break;
+                dp[i] = Math.min(dp[i], dp[i - square] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
