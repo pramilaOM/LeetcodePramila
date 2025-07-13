6cde6547fbf85a411b81eef46b0429bf40e62b69
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-13 03:58:18 -0400

    Time: 10 ms (80.18%), Space: 48.5 MB (59.7%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i = 0, j = 0, count = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                i++;
+                count++;
+            }
+            j++;
+
+        }
+        return count;
+    }
+}
