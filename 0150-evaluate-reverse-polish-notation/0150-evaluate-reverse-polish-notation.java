593964269ac92ccd3c4d49abe85dea0b8e74c829
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-18 16:26:48 -0400

    Time: 9 ms (8.42%), Space: 45 MB (22.59%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,18 @@
 class Solution {
-    int operate(int a, int b, String token) {
-        if (token.equals("+"))
-            return a + b;
-        if (token.equals("-"))
-            return a - b;
-        if (token.equals("*"))
-            return a * b;
-        if (token.equals("/"))
-            return a / b;
-        return -1;
-   
-    }
-
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
+        Map<String, BiFunction<Integer, Integer, Integer>> mp = new HashMap<>();
+        mp.put("+", (a, b) -> a + b);
+        mp.put("-", (a, b) -> a - b);
+        mp.put("*", (a, b) -> a * b);
+        mp.put("/", (a, b) -> a / b);
         for (String token : tokens) {
             if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
-                int b = stack.peek();
-                stack.pop();
+                int b = stack.pop();
 
-                int a = stack.peek();
-                stack.pop();
+                int a = stack.pop();
 
-                int result = operate(a, b, token);
+                int result = mp.get(token).apply(a, b);
                 stack.push(result);
             } else {
                 stack.push(Integer.parseInt(token));
