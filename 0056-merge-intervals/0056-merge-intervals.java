f68b6b1ecde1490e3d05e2122f85f52f2e79b0ef
pramilaOM <pramilayadavomswami@gmail.com>
2025-07-19 14:39:56 -0400

    Time: 9 ms (45.52%), Space: 47 MB (15.93%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,16 +1,9 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-
-        if (intervals.length <= 1) {
-            return intervals;
-        }
-
         Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
-
         List<int[]> result = new ArrayList<>();
         int[] newInterval = intervals[0];
         result.add(newInterval);
-
         for (int[] interval : intervals) {
             if (interval[0] <= newInterval[1]) {
                 newInterval[1] = Math.max(newInterval[1], interval[1]);
