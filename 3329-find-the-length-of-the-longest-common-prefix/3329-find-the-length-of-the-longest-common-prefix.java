1fc569f3c6c82ecaccda2e4d9a590d44e3fd92c1
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-01 20:36:32 -0700

    Time: 35 ms (85.41%), Space: 55.3 MB (91.97%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
@@ -1,26 +1,56 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[10];
+}
+
 class Solution {
-    public int longestCommonPrefix(int[] arr1, int[] arr2) {
 
-        Set<Integer> set = new HashSet<>();
-        for (int val : arr1) {
-            while (!set.contains(val) && val > 0) {
-                set.add(val);
-                val /= 10;
+    public TrieNode getTrieNode() {
+        return new TrieNode();
+    }
+
+    public void insert(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numStr = Integer.toString(num);
+
+        for (char ch : numStr.toCharArray()) {
+            int idx = ch - '0';
+            if (crawl.children[idx] == null) {
+                crawl.children[idx] = getTrieNode();
             }
+            crawl = crawl.children[idx];
         }
-
-        int result = 0;
-        for (int num : arr2) {
-            while (!set.contains(num) && num > 0) {
-                num /= 10;
-            }
-            if (num > 0) {
-                result = Math.max(result, (int) (Math.log10(num) + 1));
-
-            }
-
-        }
-        return result;
 
     }
+
+    public int search(int num, TrieNode root) {
+        TrieNode crawl = root;
+        String numStr = Integer.toString(num);
+        int len = 0;
+
+        for (char ch : numStr.toCharArray()) {
+            int idx = ch - '0';
+            if (crawl.children[idx] != null) {
+                len++;
+                crawl = crawl.children[idx];
+            } else {
+                break;
+            }
+        }
+        return len;
+    }
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        TrieNode root = getTrieNode();
+
+        for (int num : arr1) {
+            insert(num, root);
+        }
+        int result = 0;
+        for (int val : arr2) {
+            result = Math.max(result, search(val, root));
+        }
+
+        return result;
+    }
+
 }
