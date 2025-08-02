4c136f4f1490461047f7ae17e72ee4bd46310797
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-02 14:12:23 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for (char mag : magazine.toCharArray()) {
+            map.put(mag, map.getOrDefault(mag, 0) + 1);
+        }
+        for (char ran : ransomNote.toCharArray()) {
+            map2.put(ran, map2.getOrDefault(ran, 0) + 1);
+        }
+        return map.equals(map2);
+
     }
 }
