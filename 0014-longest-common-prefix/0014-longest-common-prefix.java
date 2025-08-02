24a28525b80d99837c9635deae13a1abe14ddc07
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-01 23:43:25 -0700

    Time: 1 ms (62.99%), Space: 42.5 MB (5.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,18 +1,64 @@
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        StringBuilder result = new StringBuilder();
-        Arrays.sort(strs);
-        int len = strs.length - 1;
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord = false;
 
-        String first = strs[0];
-        String last = strs[len];
-
-        for (int i = 0; i < first.length(); i++) {
-            if (first.charAt(i) != last.charAt(i)) {
-                break;
-            }
-            result.append(first.charAt(i));
+    int getChildrenCount() {
+        int count = 0;
+        for (TrieNode child : children) {
+            if (child != null)
+                count++;
         }
-        return result.toString();
+        return count;
+    }
+
+    TrieNode getSingleChild() {
+        for (TrieNode child : children) {
+            if (child != null)
+                return child;
+        }
+        return null;
+    }
+
+    int getSingleChildIndex() {
+        for (int i = 0; i < 26; i++) {
+            if (children[i] != null)
+                return i;
+        }
+        return -1;
+    }
+}
+
+class Solution {
+    private TrieNode root = new TrieNode();
+
+    private void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null) {
+                node.children[idx] = new TrieNode();
+            }
+            node = node.children[idx];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        for (String word : strs) {
+            insert(word);
+        }
+
+        TrieNode node = root;
+        StringBuilder prefix = new StringBuilder();
+
+        while (node.getChildrenCount() == 1 && !node.isEndOfWord) {
+            int index = node.getSingleChildIndex();
+            node = node.children[index];
+            prefix.append((char) (index + 'a'));
+        }
+        return prefix.toString();
     }
 }
