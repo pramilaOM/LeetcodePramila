5696df999db13211ad79c5b8207fc2496b36f6ef
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-02 20:08:33 -0700

    Time: 23 ms (10.04%), Space: 42.5 MB (52.3%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,28 +1,21 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        //T.C : O(n)
-        //S.C : O(1) -> Take array of size 256 to handle all ASCII characters instead of taking map
-
-        int[] arr1 = new int[256];
-        int[] arr2 = new int[256];
-        Arrays.fill(arr1, -1);
-        Arrays.fill(arr2, -1);
+        Map<Character, Character> mp1 = new HashMap<>();
+        Map<Character, Character> mp2 = new HashMap<>();
 
         int n = s.length();
 
         for (int i = 0; i < n; i++) {
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
 
-            if (arr1[c1] != -1 && arr1[c1] != c2 || arr2[c2] != -1 && arr2[c2] != c1) {
+            if (mp1.containsKey(ch1) && mp1.get(ch1) != ch2 || mp2.containsKey(ch2) && mp2.get(ch2) != ch1) {
                 return false;
             }
 
-            arr1[c1] = c2;
-            arr2[c2] = c1;
+            mp1.put(ch1, ch2);
+            mp2.put(ch2, ch1);
         }
-
         return true;
-
     }
 }
