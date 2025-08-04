710ee32b01bd39a7a2bf4d65b64531da792057dd
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-03 17:42:24 -0700

    Time: 1 ms (79.84%), Space: 41.6 MB (46.99%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -6,19 +6,18 @@ class Solution {
         if (words.length != n)
             return false;
 
-        Map<String, Character> wordToChar = new HashMap<>();
-        Set<Character> usedChars = new HashSet<>();
+        Map<Character, Integer> charToIndex = new HashMap<>();
+        Map<String, Integer> wordToIndex = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            String word = words[i];
             char ch = pattern.charAt(i);
+            String word = words[i];
 
-            if (!wordToChar.containsKey(word) && !usedChars.contains(ch)) {
-                wordToChar.put(word, ch);
-                usedChars.add(ch);
-            } else if (!Objects.equals(wordToChar.get(word), ch)) {
+            if (!Objects.equals(charToIndex.getOrDefault(ch, 0), wordToIndex.getOrDefault(word, 0))) {
                 return false;
             }
+            charToIndex.put(ch, i + 1);
+            wordToIndex.put(word, i + 1);
         }
         return true;
     }
