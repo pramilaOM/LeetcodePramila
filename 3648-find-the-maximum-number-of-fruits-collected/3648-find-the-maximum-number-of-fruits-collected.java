93a4b09f65fb51775e68cd4b6d410fb6bb46378a
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-07 13:44:12 -0700

    Time: 15 ms (52.38%), Space: 197.9 MB (42.86%) - LeetHub

---
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maxCollectedFruits(int[][] fruits) {
+        int n = fruits.length;
+        int[][] t = new int[n][n];
+        // t[i][j] = max fruits collected till [i][j]
+
+        // child1Collect - Diagonal elements
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += fruits[i][i];
+        }
+
+        // Before child2 and child3, nullify the cells which can't be visited by child2 and child3
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i < j && i + j < n - 1) {
+                    t[i][j] = 0;
+                } else if (i > j && i + j < n - 1) {
+                    t[i][j] = 0;
+                } else {
+                    t[i][j] = fruits[i][j];
+                }
+            }
+        }
+
+        // child2 collect fruits
+        // cells upper to diagonal : i < j
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                t[i][j] += Math.max(t[i - 1][j - 1], Math.max(t[i - 1][j], (j + 1 < n) ? t[i - 1][j + 1] : 0));
+            }
+        }
+
+        // child3 collect fruits
+        // cells below diagonal : i > j
+        for (int j = 1; j < n; j++) {
+            for (int i = j + 1; i < n; i++) {
+                t[i][j] += Math.max(t[i - 1][j - 1], Math.max(t[i][j - 1], (i + 1 < n) ? t[i + 1][j - 1] : 0));
+            }
+        }
+
+        return result + t[n - 2][n - 1] + t[n - 1][n - 2];
+    }
+}
