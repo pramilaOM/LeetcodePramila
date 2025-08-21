4fa328aebb625a7ab285fa7e0f75c272e3353f3f
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-21 16:08:01 -0400

    Time: 1 ms (99.57%), Space: 42 MB (46.67%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,13 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-         int sum_s = 0;
-        int sum_t = 0;
-        for (char ch : s.toCharArray()) {
-            sum_s += ch;
-        }
+        int sum = 0;
+
         for (char ch : t.toCharArray()) {
-            sum_t += ch;
+            sum += ch;
         }
-        return (char) (sum_t - sum_s);
+        for (char ch : s.toCharArray()) {
+            sum -= ch;
+        }
+        return (char) (sum);
     }
 }
