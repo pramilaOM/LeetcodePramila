2e9cf80c766dccfa5e99396c90d945d7cd2758ef
pramilaOM <pramilayadavomswami@gmail.com>
2025-08-29 23:26:30 -0400

    Time: 2 ms (77.42%), Space: 44.9 MB (31.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,9 +1,7 @@
-import java.util.HashSet;
-
 class Solution {
-    private boolean isValidSubBox(char[][] board, int sr, int er, int sc, int ec) {
-        HashSet<Character> set = new HashSet<>();
-        for (int row = sr; row <= er; row++) {
+    boolean isValidSubbox(char[][] board, int sr, int er, int sc, int ec) {
+        Set<Character> set = new HashSet<>();
+        for (int row = sr; row <=er; row++) {
             for (int col = sc; col <= ec; col++) {
                 char ch = board[row][col];
                 if (ch == '.')
@@ -11,15 +9,16 @@ class Solution {
                 if (set.contains(ch))
                     return false;
                 set.add(ch);
+
             }
         }
         return true;
     }
 
     public boolean isValidSudoku(char[][] board) {
-        // Validate rows
+        //row
         for (int row = 0; row < 9; row++) {
-            HashSet<Character> set = new HashSet<>();
+            Set<Character> set = new HashSet<>();
             for (int col = 0; col < 9; col++) {
                 char ch = board[row][col];
                 if (ch == '.')
@@ -27,12 +26,12 @@ class Solution {
                 if (set.contains(ch))
                     return false;
                 set.add(ch);
+
             }
         }
-
-        // Validate columns
+        //column
         for (int col = 0; col < 9; col++) {
-            HashSet<Character> set = new HashSet<>();
+            Set<Character> set = new HashSet<>();
             for (int row = 0; row < 9; row++) {
                 char ch = board[row][col];
                 if (ch == '.')
@@ -40,18 +39,19 @@ class Solution {
                 if (set.contains(ch))
                     return false;
                 set.add(ch);
+
             }
         }
-
-        // Validate 3x3 sub-boxes
-        for (int sr = 0; sr < 9; sr += 3) {
-            for (int sc = 0; sc < 9; sc += 3) {
-                if (!isValidSubBox(board, sr, sr + 2, sc, sc + 2)) {
+        //grid
+        for (int row = 0; row < 9; row += 3) {
+            for (int col = 0; col < 9; col += 3) {
+                if (!isValidSubbox(board, row, row + 2, col, col + 2)) {
                     return false;
                 }
+
             }
         }
-
         return true;
+
     }
 }
