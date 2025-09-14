ea55c41d2bdef8e323e1bfae0f9a08be7b0d10cf
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 16:26:39 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,52 +1,27 @@
 class Solution {
-    //memo
-    int m, n, p;
-    int[][][] t;
+    //bf
+    int m, n;
 
-    public boolean solve(int i, int j, int k, String s1, String s2, String s3) {
-        if (i == m && j == n && k == p) {
+    public boolean solve(int i, int j, String s1, String s2, String s3) {
+        if (i == m && j == n) {
             return true;
         }
-
-        if (t[i][j][k] != -1) {
-            return t[i][j][k] == 1;
-        }
-
-        if (k >= p) {
-            return false;
-        }
-
         boolean result = false;
-        if (i < m && s1.charAt(i) == s3.charAt(k)) {
-            result = solve(i + 1, j, k + 1, s1, s2, s3);
+        if (i < m && s1.charAt(i) == s3.charAt(i+j)) {
+            result = solve(i + 1, j, s1, s2, s3);
         }
-
-        if (result == true) {
-            t[i][j][k] = 1;
+        if(result == true){
             return true;
         }
-
-        if (j < n && s2.charAt(j) == s3.charAt(k)) {
-            result = solve(i, j + 1, k + 1, s1, s2, s3);
+        if (j < n && s2.charAt(j) == s3.charAt(i+j)) {
+            result = solve(i, j + 1, s1, s2, s3);
         }
-        t[i][j][k] = result ? 1 : 0;
         return result;
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
         m = s1.length();
         n = s2.length();
-        p = s3.length();
-        if (m + n != p) {
-            return false;
-        }
-
-        t = new int[m + 1][n + 1][p + 1];
-        for (int[][] arr : t) {
-            for (int[] a : arr) {
-                Arrays.fill(a, -1);
-            }
-        }
-        return solve(0, 0, 0, s1, s2, s3);
+        return solve(0, 0, s1, s2, s3);
     }
 }
