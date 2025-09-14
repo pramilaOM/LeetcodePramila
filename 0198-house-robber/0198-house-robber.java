7d22298756e4ac1c7cc3397688a56ef62deb651b
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-13 20:33:25 -0400

    Time: 0 ms (100%), Space: 41.3 MB (35.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,18 @@
 class Solution {
-    //Memoi Top Down
-    int[] t = new int[101];
-    public int solve(int[] nums, int index, int n) {
-        if (index >= n) {
-            return 0;
-        }
-
-        if(t[index] !=-1){
-            return t[index];
-        }
-
-        int steal = nums[index] + solve(nums, index + 2, n);
-        int skip = solve(nums, index + 1, n);
-
-        return t[index] =Math.max(steal, skip);
-    }
-
     public int rob(int[] nums) {
         int n = nums.length;
-        Arrays.fill(t,-1);
-        return solve(nums, 0, n);
+        int t[] = new int[n + 1];
+        Arrays.fill(t, 0);
+        t[0] = 0;
+        t[1] = nums[0];
+
+        for (int i = 2; i <= nums.length; i++) {
+            int steal = nums[i - 1] + t[i - 2];
+            int skip = t[i - 1];
+
+            t[i] = Math.max(steal, skip);
+        }
+
+        return t[n];
     }
 }
