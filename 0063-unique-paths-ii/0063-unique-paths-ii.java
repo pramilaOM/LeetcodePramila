800b9a8866e46d43853984747ec633bed15bdd2a
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 14:17:10 -0400

    Time: 0 ms (100%), Space: 42.4 MB (6.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,27 +1,30 @@
 class Solution {
-    Integer t[][] = new Integer[101][101];
-    int m, n;
+    int[][] t;
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        m = obstacleGrid.length;
-        n = obstacleGrid[0].length;
-
-        return solve(obstacleGrid, 0, 0);
+        //memo
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        t = new int[m + 1][n + 1];
+        for (int[] arr : t) {
+            Arrays.fill(arr, -1);
+        }
+        return solve(obstacleGrid, 0, 0, m, n);
     }
 
-    int solve(int[][] obstacleGrid,int i,int j){
-        if(i>=m || j>=n || obstacleGrid[i][j] !=0){
-            return 0;
+    public int solve(int[][] obstacleGrid, int i, int j, int m, int n) {
+        if (i >= m || j >= n || obstacleGrid[i][j] == 1) {
+            return t[i][j] = 0;
         }
-        if(t[i][j] != null) {
+        if (t[i][j] != -1) {
             return t[i][j];
         }
+        if (i == m - 1 && j == n - 1) {
+            return t[i][j] = 1;
+        }
+        int right = solve(obstacleGrid, i, j + 1, m, n);
+        int down = solve(obstacleGrid, i + 1, j, m, n);
 
-        if(i ==m-1 && j ==n-1) return 1;
-
-        int right = solve(obstacleGrid,i,j+1);
-        int down = solve(obstacleGrid,i+1,j);
-
-        return t[i][j] = right+ down;
+        return t[i][j] = right + down;
     }
 }
