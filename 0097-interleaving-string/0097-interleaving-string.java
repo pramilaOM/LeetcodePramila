d04fc8ab277336bc5cd333811e7bb473bde0047e
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 16:28:07 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,19 +1,22 @@
 class Solution {
     //bf
-    int m, n;
+    int m, n, p;
 
     public boolean solve(int i, int j, String s1, String s2, String s3) {
         if (i == m && j == n) {
             return true;
         }
+        if (i + j >= p) {
+            return false;
+        }
         boolean result = false;
-        if (i < m && s1.charAt(i) == s3.charAt(i+j)) {
+        if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
             result = solve(i + 1, j, s1, s2, s3);
         }
-        if(result == true){
+        if (result == true) {
             return true;
         }
-        if (j < n && s2.charAt(j) == s3.charAt(i+j)) {
+        if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
             result = solve(i, j + 1, s1, s2, s3);
         }
         return result;
@@ -22,6 +25,7 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         m = s1.length();
         n = s2.length();
+        p = s3.length();
         return solve(0, 0, s1, s2, s3);
     }
 }
