9fdc5674a319535f219f178be40b65068b404470
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 17:51:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,38 +1,28 @@
 class Solution {
 
-    int m, n;
-    //memo
-    int[][] t;
-
-    public int solve(String s1, String s2, int i, int j) {
-        if (i == m) {
-            return n - j;
-        } else if (j == n) {
-            return m - i;
+    //bf
+    public int solve(String s1, String s2, int m, int n) {
+        if (m == 0) {
+            return n;
+        } else if (n == 0) {
+            return m;
         }
 
-        if (t[i][j] != -1) {
-            return t[i][j];
-        }
-        if (s1.charAt(i) == s2.charAt(j)) {
-            return t[i][j] = solve(s1, s2, i + 1, j + 1);
+        if (s1.charAt(m-1) == s2.charAt(n-1)) {
+            return solve(s1, s2, m - 1, n - 1);
         }
 
-        int insert = 1 + solve(s1, s2, i, j + 1);
-        int delete = 1 + solve(s1, s2, i + 1, j);
-        int replace = 1 + solve(s1, s2, i + 1, j + 1);
+        int insert = 1 + solve(s1, s2, m, n - 1);
+        int delete = 1 + solve(s1, s2, m - 1, n);
+        int replace = 1 + solve(s1, s2, m - 1, n - 1);
 
-        return t[i][j] = Math.min(Math.min(insert, delete), replace);
+        return Math.min(Math.min(insert, delete), replace);
 
     }
 
     public int minDistance(String word1, String word2) {
-        m = word1.length();
-        n = word2.length();
-        t = new int[m + 1][n + 1];
-        for (int[] arr : t) {
-            Arrays.fill(arr, -1);
-        }
-        return solve(word1, word2, 0, 0);
+        int m = word1.length();
+        int n = word2.length();
+        return solve(word1, word2, m, n);
     }
 }
