d2584032a62d8113e9328e6c3e7d6da75986277a
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 16:18:37 -0400

    Time: 10 ms (8.05%), Space: 57.3 MB (5.02%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,44 +1,50 @@
 class Solution {
-    int m, n, N;
-    Boolean dp[][];
+    //memo
+    int m, n, p;
+    int[][][] t;
 
-    boolean check(String s1, String s2, String s3, int i, int j) {
-        if (i >= m && j >= n && i + j >= N)
+    public boolean solve(int i, int j, int k, String s1, String s2, String s3) {
+        if (i == m && j == n && k == p) {
             return true;
+        }
 
-        if (i + j >= N)
+        if (t[i][j][k] != -1) {
+            return t[i][j][k] == 1;
+        }
+
+        if (k >= p) {
             return false;
-
-        if (dp[i][j] != null)
-            return dp[i][j];
+        }
 
         boolean result = false;
-
-        if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
-            result = check(s1, s2, s3, i + 1, j);
+        if (i < m && s1.charAt(i) == s3.charAt(k)) {
+            result = solve(i + 1, j, k + 1, s1, s2, s3);
         }
 
         if (result == true) {
-            return dp[i][j] = result;
+            return true;
         }
-
-        if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
-            result = check(s1, s2, s3, i, j + 1);
+        if (j < n && s2.charAt(j) == s3.charAt(k)) {
+            result = solve(i, j + 1, k + 1, s1, s2, s3);
         }
-
-        return dp[i][j] = result;
+        t[i][j][k] = result ? 1 : 0;
+        return result;
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-
         m = s1.length();
         n = s2.length();
-        N = s3.length();
-
-        if (m + n != N)
+        p = s3.length();
+        if (m + n != p) {
             return false;
+        }
 
-        dp = new Boolean[m + 1][n + 1];
-        return check(s1, s2, s3, 0, 0);
+        t = new int[m + 1][n + 1][p + 1];
+        for (int[][] arr : t) {
+            for (int[] a : arr) {
+                Arrays.fill(a, -1);
+            }
+        }
+        return solve(0, 0, 0, s1, s2, s3);
     }
 }
