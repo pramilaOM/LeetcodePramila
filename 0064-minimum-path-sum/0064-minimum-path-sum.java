b71b20d3a4b85a9b26bb516923b7de330258a30e
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 13:52:07 -0400

    Time: 4 ms (59.47%), Space: 47.5 MB (81.51%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,34 +1,30 @@
 class Solution {
-    //memo
-    int[][] t;
-
-    public int solve(int[][] grid, int i, int j, int row, int column) {
-        if (i == row - 1 && j == column - 1) {
-            return grid[i][j];
-        }
-        if (t[i][j] != -1) {
-            return t[i][j];
-        }
-        if (i == row - 1) {//right
-            return t[i][j] = grid[i][j] + solve(grid, i, j + 1, row, column);
-        } else if (j == column - 1) {//down
-            return t[i][j] = grid[i][j] + solve(grid, i + 1, j, row, column);
-        } else {
-            return t[i][j] = grid[i][j]
-                    + Math.min(solve(grid, i, j + 1, row, column), solve(grid, i + 1, j, row, column));
-        }
-
+    public int minPathSum(int[][] grid) {
+        //bottom up
+        int m = grid.length;
+        int n = grid[0].length;
+        return solve(grid, m, n);
     }
 
-    public int minPathSum(int[][] grid) {
-        //BF
-        int row = grid.length;
-        int column = grid[0].length;
-        t = new int[row + 1][column + 1];
-        for (int[] arr : t) {
-            Arrays.fill(arr, -1);
-        }
-        return solve(grid, 0, 0, row, column);
+    public int solve(int[][] grid, int m, int n) {
+        int[][] t = new int[m][n];
+        t[0][0] = grid[0][0];
 
+        //first row
+        for (int i = 1; i < n; i++) {
+            t[0][i] = grid[0][i] + t[0][i - 1];
+        }
+        //first column
+        for (int i = 1; i < m; i++) {
+            t[i][0] = grid[i][0] + t[i - 1][0];
+        }
+        //remaining
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                t[i][j] = grid[i][j] + Math.min(t[i][j - 1], t[i - 1][j]);
+            }
+        }
+        return t[m - 1][n - 1];
     }
 }
