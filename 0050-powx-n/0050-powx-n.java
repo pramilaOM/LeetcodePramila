9566e09050cc52e2915eece6adf5f43c9d94e8c3
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 21:55:43 -0400

    Time: 0 ms (100%), Space: 42.2 MB (61.9%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,21 +1,13 @@
 class Solution {
+    private double solve(double x, long n){
+        if(n ==0) return 1;
+        if(n < 0) return solve(1/x,-n);
 
-    double solve(double x, long n) {
-        if (n == 0)
-            return 1;
+        if(n%2 ==0) return solve(x*x,n/2);
 
-        if (n < 1)
-            return solve(1 / x, -n);
-
-        if (n % 2 == 0)
-            return solve(x * x, n / 2);
-
-        return x * solve(x * x, (n - 1) / 2);
+        return x*solve(x*x,(n-1)/2);
     }
-
     public double myPow(double x, int n) {
-
-        return solve(x, (long) n);
-
+        return solve(x,(long)n);
     }
 }
