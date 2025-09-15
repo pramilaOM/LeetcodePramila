cf2661be6b0de7c2a3e829d828f70f64408451dc
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 22:19:18 -0400

    Time: 1 ms (99.77%), Space: 42.5 MB (29.13%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,9 +1,8 @@
 class Solution {
     public String addBinary(String a, String b) {
-
         StringBuilder sb = new StringBuilder();
-        int i = a.length()-1;
-        int j = b.length()-1;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int carry = 0;
         while (i >= 0 || j >= 0 || carry == 1) {
             int sum = carry;
@@ -18,6 +17,7 @@ class Solution {
             }
             sb.append(sum % 2);
             carry = sum / 2;
+
         }
         return sb.reverse().toString();
 
