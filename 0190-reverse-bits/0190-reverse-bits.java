56fa9552e8c7ebd09ebcecc2edf64cf3c553674f
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-14 23:16:44 -0400

    Time: 0 ms (100%), Space: 41 MB (42.46%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,27 +1,15 @@
-public class Solution {
-    private final int[] reverseByte = new int[256];
-
-    // Constructor: fill the reverse lookup table
-    public Solution() {
-        for (int i = 0; i < 256; i++) {
-            reverseByte[i] = reverseByte(i);
-        }
-    }
-
-    // Reverse the bits in a byte (8-bit)
-    private int reverseByte(int b) {
-        int res = 0;
-        for (int i = 0; i < 8; i++) {
-            res |= ((b >> i) & 1) << (7 - i);
-        }
-        return res;
-    }
-
+class Solution {
     public int reverseBits(int n) {
-        // Break 32-bit int into 4 bytes and reverse each
-        return (reverseByte[n & 0xff] << 24) |
-               (reverseByte[(n >>> 8) & 0xff] << 16) |
-               (reverseByte[(n >>> 16) & 0xff] << 8) |
-               (reverseByte[(n >>> 24) & 0xff]);
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            if ((n & 1) == 1)
+                result++;
+
+            n >>= 1;
+
+        }
+        return result;
+
     }
 }
