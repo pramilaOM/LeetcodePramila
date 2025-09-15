97f951fb9d36ed9e73394e96cf9205b533375855
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-15 15:04:46 -0400

    Time: 0 ms (100%), Space: 42.1 MB (68.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,47 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+        int n = nums.length;
+        int pivotElement = findPivot(nums, 0, n-1);
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        if (nums[pivotElement] == target) {
+            return pivotElement;
+        }
+        int idx = -1;
+        idx = binarySearch(0, pivotElement - 1, nums, target);
+        if (idx != -1) {
+            return idx;
+        }
+        idx = binarySearch(pivotElement, n - 1, nums, target);
 
-            if (nums[mid] == target) {
-                return mid;
-            }
+        return idx;
+    }
 
-            if (nums[start] <= nums[mid]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
+    int findPivot(int[] nums, int l, int r) {
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
             } else {
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+                r = mid;
             }
         }
+        return r;
+    }
 
-        return -1;
+    int binarySearch(int l, int r, int[] nums, int target) {
+        int idx = -1;
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                idx = mid;
+                break;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return idx;
     }
 }
