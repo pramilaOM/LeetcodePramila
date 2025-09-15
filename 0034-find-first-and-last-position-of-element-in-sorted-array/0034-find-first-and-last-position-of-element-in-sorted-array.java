18958e581c680e9c43397d0d2034f9aea7a7a7a4
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-15 15:39:02 -0400

    Time: 0 ms (100%), Space: 46.1 MB (30.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,47 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[]{-1, -1};
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                arr[0] = i;
-
-                while (i + 1 < nums.length && nums[i + 1] == target) {
-                    i++;
-                }
-
-                arr[1] = i;
-                break;
+    int searchLeft(int[] nums, int target, int n) {
+        int l = 0, r = n - 1;
+        int left = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                left = mid;
+                r = mid - 1;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
-        if (arr[0] != -1 && arr[1] == -1) {
-            arr[1] = arr[0];
-        }
+        return left;
+    }
 
-        return arr;
+    int searchRight(int[] nums, int target, int n) {
+        int l = 0, r = n - 1;
+        int right = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+                l = mid + 1;
+            }else if(nums[mid] < target){
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+    return right;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[2];
+        int n = nums.length;
+
+        int left = searchLeft(nums, target, n);
+        int right = searchRight(nums, target, n);
+        ans[0] = left;
+        ans[1] = right;
+        return ans;
     }
 }
