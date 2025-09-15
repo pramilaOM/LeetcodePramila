bb9dea47e8323b2bd6a04ea0b3b56592edf066f4
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-15 14:02:02 -0400

    Time: 0 ms (100%), Space: 42.2 MB (66.19%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //Binary Search
         int m = matrix.length;
         int n = matrix[0].length;
-        int i = 0, j = n - 1;
 
-        while (i < m && j >= 0) {
-            if (matrix[i][j] > target) {
-                j--;
-            } else if (matrix[i][j] < target) {
-                i++;
+        int start = 0, end = m * n - 1;
+        while (start <= end) {
+            int mid = start + (end - start / 2);
+            if (matrix[mid / n][mid % n] < target) {
+                start = mid + 1;
+            } else if (matrix[mid / n][mid % n] > target) {
+                end = mid - 1;
             } else {
                 return true;
             }
