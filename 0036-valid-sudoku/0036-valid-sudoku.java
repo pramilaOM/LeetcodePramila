e842cdc2260506d62494400493ca79ee60b646d5
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-15 20:05:34 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,59 +1,22 @@
 class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
 
-    private boolean valideSubBox(char[][] board, int sr, int er, int sc, int ec) {
-        Set<Character> set = new HashSet<>();
-        for (int i = sr; i <= er; i++) {
-
-            for (int j = sc; j <= ec; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.')
                     continue;
-                if (set.contains(board[i][j]))
+
+                String rowKey = String.valueOf(board[i][j]) + "_row_" + i;
+                String columnKey = String.valueOf(board[i][j]) + "_col_" + j;
+                String subBox = String.valueOf(board[i][j]) + (i / 3) + "_box_" + (j / 3);
+
+                if (!set.contains(rowKey) || !set.contains(columnKey) || !set.contains(subBox)) {
                     return false;
-
-                set.add(board[i][j]);
-
+                }
             }
         }
         return true;
-    }
 
-    public boolean isValidSudoku(char[][] board) {
-        //bf
-        //row
-        for (int row = 0; row < 9; row++) {
-            Set<Character> set = new HashSet<>();
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.')
-                    continue;
-                if (set.contains(board[row][col]))
-                    return false;
-                set.add(board[row][col]);
-            }
-        }
-
-        //column
-        for (int col = 0; col < 9; col++) {
-            Set<Character> set = new HashSet<>();
-            for (int row = 0; row < 9; row++) {
-                if (board[row][col] == '.')
-                    continue;
-                if (set.contains(board[row][col]))
-                    return false;
-                set.add(board[row][col]);
-            }
-        }
-
-        //3*3 boxes
-
-        for (int sr = 0; sr < 9; sr += 3) {
-            int er = sr + 2;
-            for (int sc = 0; sc < 9; sc += 3) {
-                int ec = sc + 2;
-                if (!valideSubBox(board, sr, er, sc, ec))
-                    return false;
-            }
-        }
-
-        return true;
     }
 }
