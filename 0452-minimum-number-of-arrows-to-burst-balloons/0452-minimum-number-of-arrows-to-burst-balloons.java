39f0858370016bcf3e945b3711a58d06cdf686d8
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-16 15:40:56 -0400

    Time: 58 ms (16.27%), Space: 75.7 MB (14.91%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,27 +1,34 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        int n = points.length;
-        Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
 
-        int[][] balloons = new int[n][2];
-        balloons[0] = points[0];
-        int balloonIndex = 0;
+        int n = points.length;
+        if (n == 0)
+            return 0;
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] != b[0])
+                return Integer.compare(a[0], b[0]);
+            return Integer.compare(a[1], b[1]);
+        });
+
+        int[] prev = Arrays.copyOf(points[0], 2);
+        int count = 1;
 
         for (int i = 1; i < n; i++) {
-            int currStartPoint = points[i][0];
-            int currEndPoint = points[i][1];
+            int currStart = points[i][0];
+            int currEnd = points[i][1];
 
-            int prevStartPoint = balloons[balloonIndex][0];
-            int prevEndPoint = balloons[balloonIndex][1];
+            int prevStart = prev[0];
+            int prevEnd = prev[1];
 
-            if (currStartPoint > prevEndPoint) {
-                balloons[++balloonIndex] = points[i];
+            if (currStart > prevEnd) {
+                count++;
+                prev = points[i];
             } else {
-                balloons[balloonIndex][0] = Math.max(prevStartPoint, currStartPoint);
-                balloons[balloonIndex][1] = Math.min(prevEndPoint, currEndPoint);
+                prev[0] = Math.max(prevStart, currStart);
+                prev[1] = Math.min(prevEnd, currEnd);
             }
         }
-        return balloonIndex + 1;
+        return count;
 
     }
 }
