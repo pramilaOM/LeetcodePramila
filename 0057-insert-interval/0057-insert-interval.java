aa4c42a4f9b5156ff60194340713fcd42a7e7756
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-16 15:01:58 -0400

    Time: 1 ms (98.81%), Space: 45.1 MB (41.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,37 +1,25 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        //bf
         int i = 0;
-        while (i < intervals.length) {
-            if (intervals[i][1] < newInterval[0])
-                i++;
-            else if (intervals[i][0] > newInterval[1]) {
-                List<int[]> result = new ArrayList<>();
-                for (int j = 0; j < i; j++) {
-                    result.add(intervals[j]);
-                }
-                result.add(newInterval);
-                for (int j = i; j < intervals.length; j++) {
-                    result.add(intervals[j]);
-                }
-                return result.toArray(new int[result.size()][]);
+        List<int[]> result = new ArrayList<>();
+        int n = intervals.length;
+
+        while (i < n) {
+            if (intervals[i][1] < newInterval[0]) {
+                result.add(intervals[i]);
+            } else if (intervals[i][0] > newInterval[1]) {
+                break;
             } else {
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
                 newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-                List<int[]> tempList = new ArrayList<>();
-                for (int j = 0; j < intervals.length; j++) {
-                    if (j != i) {
-                        tempList.add(intervals[j]);
-                    }
-                }
-                intervals = tempList.toArray(new int[tempList.size()][]);
             }
-        }
-        List<int[]> result = new ArrayList<>();
-        for (int[] interval : intervals) {
-            result.add(interval);
+            i++;
         }
         result.add(newInterval);
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
         return result.toArray(new int[result.size()][]);
     }
 }
