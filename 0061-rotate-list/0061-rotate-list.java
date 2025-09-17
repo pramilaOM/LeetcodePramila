1a2f23fededbb7dd3bc8508a9e6ee97646ef0f3f
pramilaOM <pramilayadavomswami@gmail.com>
2025-09-16 20:38:25 -0400

    Time: 0 ms (100%), Space: 42.5 MB (65.01%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -8,26 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//https://www.youtube.com/watch?v=QxevnZTKvXc
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null) {
+        int length = getListLength(head);
+
+        if (length <= 1 || k == 0 || k % length == 0) {
             return head;
         }
-        int n;
-        ListNode tail = head;
-        for (n = 1; tail.next != null; n++) {
-            tail = tail.next;
-        }
-        tail.next = head;
-        k %= n;
-        ListNode newTail = head;
-        ListNode newhead = head;
-        for (int i = 1; i < n - k; i++) {
-            newTail = newTail.next;
+        if (k > length) {
+            k = k % length;
         }
 
-        newhead = newTail.next;
-        newTail.next = null;
-        return newhead;
+        int breakagePoint = length - k;
+        ListNode temp = head;
+        ListNode prev = null;
+        while (breakagePoint > 0 && temp != null) {
+            prev = temp;
+            temp = temp.next;
+            breakagePoint--;
+        }
+        prev.next = null;
+        ListNode iterator = temp;
+        if (iterator != null) {
+            while (iterator.next != null) {
+                iterator = iterator.next;
+            }
+            iterator.next = head;
+        }
+        return temp;
+    }
+
+    int getListLength(ListNode head) {
+        int count = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
     }
 }
