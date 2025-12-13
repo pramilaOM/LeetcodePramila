b78b9f3620f398b0895baf5a2511ee2e9b016f84
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-13 15:16:30 -0500

    Time: 2 ms (51.73%), Space: 94.5 MB (30.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,24 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // Optimized Approach — One Pass (Kadane-like idea)
-        // Time Complexity:
-        // O(n) — single pass through array
 
-        // \U0001f9e0 Space Complexity:
-        // O(1) — constant space
-
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if(prices[i] < minPrice){
-                minPrice = prices[i];
-            }
-            int currProfit = prices[i]-minPrice;
-            if(currProfit > maxProfit){
-                maxProfit = currProfit;
+        int buyPrice = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            } else {
+                int currentProfit = prices[i] - buyPrice;
+                profit = Math.max(profit, currentProfit);
             }
         }
-        return maxProfit;
-      
+
+        return profit;
+
     }
 }
