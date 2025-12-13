e78ea1cfdae1b2619f1bc22df0e9210bdc08f56e
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-13 07:44:50 -0500

    Time: 1 ms (62.03%), Space: 43.3 MB (30.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,64 +1,23 @@
-class TrieNode {
-    TrieNode[] children = new TrieNode[26];
-    boolean isEndOfWord = false;
-
-    int getChildrenCount() {
-        int count = 0;
-        for (TrieNode child : children) {
-            if (child != null)
-                count++;
-        }
-        return count;
-    }
-
-    TrieNode getSingleChild() {
-        for (TrieNode child : children) {
-            if (child != null)
-                return child;
-        }
-        return null;
-    }
-
-    int getSingleChildIndex() {
-        for (int i = 0; i < 26; i++) {
-            if (children[i] != null)
-                return i;
-        }
-        return -1;
-    }
-}
-
 class Solution {
-    private TrieNode root = new TrieNode();
-
-    private void insert(String word) {
-        TrieNode node = root;
-        for (char ch : word.toCharArray()) {
-            int idx = ch - 'a';
-            if (node.children[idx] == null) {
-                node.children[idx] = new TrieNode();
-            }
-            node = node.children[idx];
-        }
-        node.isEndOfWord = true;
-    }
-
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0)
+
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        int index =0;
+        while(index < first.length()){
+            if(first.charAt(index) == last.charAt(index)){
+                index++;
+            }else{
+                break;
+            }
+        }
+        if(index == 0){
             return "";
-
-        for (String word : strs) {
-            insert(word);
         }
-
-        TrieNode node = root;
-        StringBuilder prefix = new StringBuilder();
-
-        while (node.getChildrenCount() == 1 && !node.isEndOfWord) {
-            int index = node.getSingleChildIndex();
-            node = node.children[index];
-            prefix.append((char) (index + 'a'));
+        else{
+            return first.substring(0,index);
         }
-        return prefix.toString();
+        
     }
 }
