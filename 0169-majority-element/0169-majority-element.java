6efe5d6565663a62825c1ed29c06f7632fe9ecaa
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-13 15:33:46 -0500

    Time: 1 ms (99.84%), Space: 55.6 MB (29.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,23 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-// Optimized Approach — Boyer-Moore Voting Algorithm
-        // ⏱ Time Complexity:
-        // O(n) — single pass through the array
-        // \U0001f9e0 Space Complexity:
-        // O(1) — constant space (no extra structures used)
-        int counter = 0;
-        int candidate = -1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (counter == 0) {
-                candidate = nums[i];
-            }
-            if (nums[i] == candidate) {
-                counter++;
+        int count = 1;
+        int majority = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == majority) {
+                count++;
+            } else if (count == 0) {
+                majority = nums[i];
+                count = 1;
             } else {
-                counter--;
+                count--;
             }
         }
-        return candidate;
+        return majority;
+
     }
 }
