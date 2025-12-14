ba9bc80d49dd5ef310604f4573b6d69a0c878bfe
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-14 08:44:52 -0500

    Time: 0 ms (100%), Space: 43.3 MB (8.01%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,15 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        int n = s.length();
-        int index = 0;
-        for(int i = n-1;i>=0;i--){
-            if(s.charAt(i) == ' '){
+        s= s.trim();
+        int count = 0;
+        StringBuilder sb = new StringBuilder(s);
+        for(int i = sb.length()-1; i>=0;i--){
+            if(sb.charAt(i) == ' '){
                 break;
             }
-            index++;
+            count++;
         }
-        return index;
+        return count;
+        
     }
 }
