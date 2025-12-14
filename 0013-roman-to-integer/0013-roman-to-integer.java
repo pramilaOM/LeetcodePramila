e6dd2794ba9b8823587eb8fc706f61d689f1496f
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-14 12:42:19 -0500

    Time: 4 ms (66.09%), Space: 47 MB (9.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -10,14 +10,16 @@ class Solution {
         map.put('M', 1000);
 
         int n = s.length();
-        int result = map.get(s.charAt(n - 1));
+        int ans = map.get(s.charAt(n-1));
         for (int i = n - 2; i >= 0; i--) {
-            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
-                result = result - map.get(s.charAt(i));
+            if (map.get(s.charAt(i)) >= map.get(s.charAt(i+1))) {
+                ans = ans + map.get(s.charAt(i));
             } else {
-                result = result + map.get(s.charAt(i));
+
+                ans = ans -map.get(s.charAt(i));
             }
+
         }
-        return result;
+        return ans;
     }
 }
