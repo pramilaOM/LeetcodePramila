60ff31ef0ea0d032a39422e94329660fcfe5eb29
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-13 21:30:54 -0500

    Time: 0 ms (100%), Space: 43.6 MB (50.64%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,18 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //TC O(m+n)log(m+n) 
-        //SC Sorting inplace no extra space hence O(1)
-        // add num2 in num1 TC O(n)
-        int index = 0;
-        for (int i = m; i < m + n; i++) {
-            nums1[i] = nums2[index];
-            index++;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (j >= 0) {
+            if ( i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
         }
-        //num1 has m+n element
-        // sort the element TC O(m+n)log(m+n)
-        Arrays.sort(nums1);// SC O(1)
-        for (int n1 : nums1) {
-            System.out.print(n1 + " ");
-        }
+
     }
 }
