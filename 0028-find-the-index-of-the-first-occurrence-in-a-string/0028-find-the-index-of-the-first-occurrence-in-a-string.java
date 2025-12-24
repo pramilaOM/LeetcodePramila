078f85961f669793a9429f2b89099235a544aa86
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-24 08:07:14 -0500

    Time: 3 ms (6%), Space: 43 MB (26.74%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,21 +1,54 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int m = haystack.length();
-        int n = needle.length();
-        for (int i = 0; i <= m - n; i++) {
-            int index = 0;
-            while (index < n) {
-                if (haystack.charAt(i + index) == needle.charAt(index)) {
-                    index++;
-                } else {
-                    break;
-                }
 
-            }
-            if (index == n) {
-                return i;
+        if (needle.length() == 0)
+            return 0;
+        int[] lps = buildLPS(needle);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == needle.length()) {
+                    return i - j;
+                }
+            } else {
+                if (j > 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
         return -1;
+    }
+
+    //kmp
+    private int[] buildLPS(String needle) {
+        int n = needle.length();
+        int[] lps = new int[n];
+
+        int length = 0;
+        int i = 1;
+
+        while (i < n) {
+            if (needle.charAt(i) == needle.charAt(length)) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length > 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 }
