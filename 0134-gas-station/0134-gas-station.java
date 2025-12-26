c1a5e436b71d17619985899f8bb751d8abefad55
pramilaOM <pramilayadavomswami@gmail.com>
2025-12-25 20:34:30 -0500

    Time: 2 ms (99.19%), Space: 119.6 MB (18.55%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,26 +1,29 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalKarcha = 0,totalKamayi=0;
-        for(int n : gas){
-            totalKarcha+= n;
-        }
-        for(int n : cost){
-            totalKamayi+= n;
+
+        int totalKamai = 0;
+        int totalKharcha = 0;
+        for (int a : gas) {
+            totalKamai += a;
         }
 
-        if(totalKarcha < totalKamayi ){
+        for (int b : cost) {
+            totalKharcha += b;
+        }
+
+        if (totalKamai < totalKharcha)
             return -1;
-        }
-        
+
         int total = 0;
-        int result = 0;
-        for(int i = 0;i<gas.length;i++){
-            total = total+ gas[i]-cost[i];
-            if(total < 0){
+        int index = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total = total + gas[i] - cost[i];
+            if (total < 0) {
                 total = 0;
-                result = i+1;
+                index = i + 1;
             }
+
         }
-        return result;
+        return index;
     }
 }
