058a746578a58bb2264a57e5891d7e140145a929
pramilaOM <pramilayadavomswami@gmail.com>
2026-01-26 11:18:25 -0500

    Time: 21 ms (72.27%), Space: 64.1 MB (20.2%) - LeetHub

---
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+         Arrays.sort(arr);
+
+        List<List<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+
+        int minDiff = Integer.MAX_VALUE;
+
+        // First pass: find minimum difference
+        for (int i = 1; i < n; i++) {
+            int diff = arr[i] - arr[i - 1];
+            minDiff = Math.min(minDiff, diff);
+        }
+
+        // Second pass: collect pairs with min difference
+        for (int i = 1; i < n; i++) {
+            int diff = arr[i] - arr[i - 1];
+            if (diff == minDiff) {
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+
+        return result;
+    }
+}
