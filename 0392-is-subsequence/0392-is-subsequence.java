0dc70a885e9eaea8679bc71feaf575aa9e733bc8
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-26 09:00:32 -0400

    Time: 4 ms (3.69%), Space: 43.6 MB (5.04%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,18 +3,32 @@ class Solution {
         //http://youtube.com/watch?v=r51_ZifUdvU
         int m = s.length();
         int n = t.length();
-        int start = 0, end = 0;
+        //follow up question
+        Map<Character, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(t.charAt(i), k -> new ArrayList<>()).add(i);
+        }
 
-        while (start < m && end < n) {
-            if (s.charAt(start) == t.charAt(end)) {
-                start++;
+        int prev = -1;
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
             }
-            end++;
+
+            List<Integer> indices = map.get(c);
+            int index = Collections.binarySearch(indices, prev + 1);
+
+            if (index < 0) {
+                index = -index - 1;
+            }
+
+            if (index == indices.size()) {
+                return false;
+            }
+
+            prev = indices.get(index);
 
         }
-        if (start == m) {
-            return true;
-        }
-        return false;
+        return true;
     }
 }
