3b7cc1412f5676c9ef5d9a3152285fc3129a73e1
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-28 17:47:44 -0400

    Time: 3 ms (87.35%), Space: 43.7 MB (13.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-//Use a stack to track opening brackets.
-// For every character in the string:
-// If it's an opening bracket ((, [, {), push it onto the stack.
-// If it's a closing bracket (), ], }), check:
-// If the stack is empty, it's invalid (no matching open).
-// Pop from stack and verify that it matches the closing bracket.
-// At the end, the stack should be empty if all brackets were valid and matched.
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '{' || c == '[' || c == '(') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
+        //https://www.youtube.com/watch?v=3ssh_mk4LqY
+         Stack<Character> st = new Stack<>();
 
-                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
-                    return false;
-                }
+        for (char ch : s.toCharArray()) {
+            if (st.isEmpty() || ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+                continue;
             }
 
+            if (ch == ')') {
+                if (st.peek() == '(')
+                    st.pop();
+                else
+                    return false;
+            } else if (ch == '}') {
+                if (st.peek() == '{')
+                    st.pop();
+                else
+                    return false;
+            } else if (ch == ']') {
+                if (st.peek() == '[')
+                    st.pop();
+                else
+                    return false;
+            }
         }
 
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
