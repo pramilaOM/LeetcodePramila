7710b69b63f6bd868c03a10b130581b0500b7ecd
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-28 18:14:21 -0400

    Time: 10 ms (86.08%), Space: 46.6 MB (46.97%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,13 +1,16 @@
 class Solution {
     public int calculate(String s) {
-
-        Stack<Integer> stack = new Stack<>();
-        int number = 0, result = 0, sign = 1;
+        //https://www.youtube.com/watch?v=3AEKyHx3tzU
+         Stack<Integer> st = new Stack<>();
+        int number = 0;
+        int result = 0;
+        int sign = 1;
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
+
             if (Character.isDigit(ch)) {
-                number = number * 10 + (ch - '0');
+                number = 10 * number + (ch - '0');
             } else if (ch == '+') {
                 result += sign * number;
                 number = 0;
@@ -16,21 +19,25 @@ class Solution {
                 result += sign * number;
                 number = 0;
                 sign = -1;
-
             } else if (ch == '(') {
-                stack.push(result);
-                stack.push(sign);
+                st.push(result);
+                st.push(sign);
                 result = 0;
+                number = 0;
                 sign = 1;
             } else if (ch == ')') {
                 result += sign * number;
                 number = 0;
-                result *= stack.pop();
-                result += stack.pop();
+
+                int top = st.pop();   // sign before '('
+                result *= top;
+
+                top = st.pop();       // result before '('
+                result += top;
             }
         }
+
         result += sign * number;
         return result;
-
     }
 }
