42e6e82759f3dbe2c08ba84c908dc14ff9ac6b83
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-28 15:45:55 -0400

    Time: 1 ms (82.32%), Space: 42.7 MB (66.18%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -2,23 +2,29 @@ class Solution {
     public boolean wordPattern(String pattern, String s) {
         //https://www.youtube.com/watch?v=n0cn5tJwkaE
         //https://www.youtube.com/watch?v=b_pNf56VIqY
-        String[] temp = s.split(" ");
+        HashMap<Character, Integer> charToIndex = new HashMap<>();
+        HashMap<String, Integer> wordToIndex = new HashMap<>();
+
+        String[] words = s.split(" ");
         int n = pattern.length();
 
-        if (temp.length != n) {
+        if (words.length != n) {
             return false;
         }
 
-        HashMap<String, Character> map = new HashMap<>();
-        HashSet<Character> used = new HashSet<>();
-
         for (int i = 0; i < n; i++) {
-            if (!map.containsKey(temp[i]) && !used.contains(pattern.charAt(i))) {
-                used.add(pattern.charAt(i));
-                map.put(temp[i], pattern.charAt(i));
-            } else if (!map.containsKey(temp[i]) || map.get(temp[i]) != pattern.charAt(i)) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            int charIndex = charToIndex.getOrDefault(ch, 0);
+            int wordIndex = wordToIndex.getOrDefault(word, 0);
+
+            if (charIndex != wordIndex) {
                 return false;
             }
+
+            charToIndex.put(ch, i + 1);
+            wordToIndex.put(word, i + 1);
         }
 
         return true;
