ba4254a7b862e4a5a4ee9e4a056378cc5e2488d2
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-28 17:29:46 -0400

    Time: 8 ms (91.06%), Space: 49.3 MB (33.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,17 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        //https://www.youtube.com/watch?v=9rCmrMed8h8
+        //https://leetcode.com/problems/check-if-grid-can-be-cut-into-sections/
+        //always sort interval based problem
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> result = new ArrayList<>();
-        int[] newInterval = intervals[0];
-        result.add(newInterval);
-        for (int[] interval : intervals) {
-            if (interval[0] <= newInterval[1]) {
-                newInterval[1] = Math.max(newInterval[1], interval[1]);
+        result.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int curr_start = intervals[i][0];
+            int curr_end = intervals[i][1];
+
+            if (curr_start > result.get(result.size() - 1)[1]) {
+                result.add(intervals[i]);
             } else {
-                newInterval = interval;
-                result.add(newInterval);
+                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], curr_end);
             }
         }
+
         return result.toArray(new int[result.size()][]);
+
     }
 }
