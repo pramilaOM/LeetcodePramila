08f53b1686f338b10e2f8e19bb1856e79322b244
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-28 16:27:52 -0400

    Time: 1 ms (77.96%), Space: 42.7 MB (28.94%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isHappy(int n) {
+        //https://www.youtube.com/watch?v=LkD0D0Xy-ro
+         Set<Integer> usedIntegers = new HashSet<>();
+    while (true) {
+      int sum = 0;
+      while (n != 0) {
+        sum += Math.pow(n % 10, 2.0);
+        n = n / 10;
+      }
 
-        Set<Integer> usedIntegers = new HashSet<>();
-        while (true) {
-            int sum = 0;
-            while (n != 0) {
-                sum += Math.pow(n % 10, 2);
-                n = n / 10;
-            }
-            if (sum == 1)
-                return true;
-            n = sum;
-            if (usedIntegers.contains(n))
-                return false;
-            usedIntegers.add(n);
+      if (sum == 1) return true;
 
-        }
+      n = sum;
+      if (usedIntegers.contains(n))
+        return false;
+      usedIntegers.add(n);
     }
-
+    }
 }
