61d3e6725b97bdfc672ce2eafaf7bc5d2cf6a3cd
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-28 16:58:13 -0400

    Time: 3 ms (80.24%), Space: 43.1 MB (48.72%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,25 +1,29 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        //did by myself
-        //Time Complexity: O(n), because each element is visited once.
-
-        // Space Complexity: O(1) extra (excluding result list).
-
+        //https://www.youtube.com/watch?v=PyYZCJRAMmY
         int n = nums.length;
-        List<String> ans = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+
+        if (n == 0)
+            return result;
+
         for (int i = 0; i < n; i++) {
+
             int start = nums[i];
-            while (i < n - 1 && nums[i + 1] == nums[i] + 1) {
+
+            // extend the range
+            while (i + 1 < n && nums[i] + 1 == nums[i + 1]) {
                 i++;
             }
-            int end = nums[i];
-            if (start == end) {
-                ans.add(String.valueOf(start));
-            } else {
-                ans.add(start + "->" + end);
-            }
 
+            // check if it's a range or single number
+            if (start != nums[i]) {
+                result.add(start + "->" + nums[i]);
+            } else {
+                result.add(String.valueOf(start));
+            }
         }
-        return ans;
+
+        return result;
     }
 }
