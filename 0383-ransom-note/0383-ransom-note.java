360aed21a73cf58e098e9bb27932f3c3012eb254
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-28 15:15:00 -0400

    Time: 1 ms (99.91%), Space: 46.6 MB (49.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        //Create a frequency count array freq[26] for characters in magazine.
-        int[] freq = new int[26];
-        for (char ch : magazine.toCharArray()) {
-            freq[ch - 'a']++;
+        //https://www.youtube.com/watch?v=-HkyMzzGWXM
+
+        int[] fre = new int[26];
+        for (char c : magazine.toCharArray()) {
+            fre[c - 'a']++;
         }
-        // Iterate over ransomNote:
-        for (char ran : ransomNote.toCharArray()) {
-            // For each character, check if it's available in freq[].
-            if (freq[ran -'a'] == 0) {
+        for (char c : ransomNote.toCharArray()) {
+            if (fre[c - 'a'] == 0) {
                 return false;
             }
-            // If yes, decrement count.
-            freq[ran - 'a']--;
-            // If not, return false.
+            fre[c - 'a']--;
         }
-
-        // If all characters are available in the required quantity → return true.
         return true;
     }
 }
