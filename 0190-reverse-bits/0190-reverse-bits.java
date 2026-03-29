78e1e1b8b909c4e2ed15366af179c0a3580139ad
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-29 14:52:01 -0400

    Time: 1 ms (62.87%), Space: 42.2 MB (95.15%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -2,11 +2,11 @@ class Solution {
     public int reverseBits(int n) {
         int result = 0;
         for (int i = 0; i < 32; i++) {
-            result <<= 1;
-            if ((n & 1) == 1) {
+            result <<= 1; //left shift
+            if ((n & 1) == 1) {//check last bit
                 result++;
             }
-            n >>= 1;
+            n >>= 1;// right shift
         }
         return result;
     }
