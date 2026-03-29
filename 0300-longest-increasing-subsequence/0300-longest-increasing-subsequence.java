c12920fa2565ae4a2f60a47841b6fd2b4e9f4116
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-29 13:31:23 -0400

    Time: 646 ms (5%), Space: 284.7 MB (5.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,20 @@
 class Solution {
-    int n;
-    int[][] t;
+    int[][] t = new int[2501][2501];
 
-    //memo
-    private int solve(int[] nums, int i, int prev) {
-        if (i >= n) {
+    int solve(int[] nums, int i, int prev) {
+        if (i >= nums.length) {
             return 0;
         }
 
-        if (prev != -1 && t[i][prev] != -1) {//otherwise ArrayIndexOutOfBoundsException
+        if (prev != -1 && t[i][prev] != -1) {
             return t[i][prev];
         }
-
         int take = 0;
+
         if (prev == -1 || nums[i] > nums[prev]) {
             take = 1 + solve(nums, i + 1, i);
         }
         int skip = solve(nums, i + 1, prev);
-
         if (prev != -1) {
             t[i][prev] = Math.max(take, skip);
         }
@@ -25,12 +22,11 @@ class Solution {
     }
 
     public int lengthOfLIS(int[] nums) {
-        n = nums.length;
-        t = new int[2501][2501];
-        for (int[] arr : t) {
+        for (int arr[] : t) {
             Arrays.fill(arr, -1);
-        }
 
+        }
         return solve(nums, 0, -1);
+
     }
 }
