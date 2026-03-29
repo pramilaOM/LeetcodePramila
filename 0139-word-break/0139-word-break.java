416aab99035fadc47e0cdafcc0c44a78d0b6eda3
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-03-29 11:17:32 -0400

    Time: 5 ms (66.14%), Space: 44.6 MB (63.09%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,28 +1,15 @@
 class Solution {
-    Boolean[] t = new Boolean[301];
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>();
+        wordSet.addAll(wordDict);
 
-    boolean solve(String s, List<String> wordDict, int idx) {
-
-        if (idx == s.length()) {
-            return t[idx] = true;
-
-        }
-
-        if (t[idx] != null) {
-            return t[idx];
-        }
-
-        for (int i = idx + 1; i <= s.length(); i++) {
-            String sub = s.substring(idx, i);
-            if (wordDict.contains(sub) && solve(s, wordDict, i)) {
-                return t[idx] = true;
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i < dp.length; i++) {
+            for (int k = 1; k <= i; k++) {
+                dp[i] = dp[i] || (dp[i - k] && wordSet.contains(s.substring(i - k, i)));
             }
         }
-
-        return t[idx] = false;
-    }
-
-    public boolean wordBreak(String s, List<String> wordDict) {
-        return solve(s, wordDict, 0);
+        return dp[s.length()];
     }
 }
