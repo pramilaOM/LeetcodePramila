15ee79bb8246332c728606b16962b9eb76302111
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-04-05 16:47:40 -0400

    Time: 0 ms (100%), Space: 43 MB (41.83%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -20,9 +20,34 @@ class Solution {
             return true;
         if (p == null || q == null)
             return false;
-        if (p.val != q.val) {
-            return false;
+
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.add(p);
+        q2.add(q);
+
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode first = q1.poll();
+            TreeNode second = q2.poll();
+
+            if (first.val != second.val)
+                return false;
+
+            if (first.left != null && second.left != null) {
+                q1.add(first.left);
+                q2.add(second.left);
+            } else if (first.left != null || second.left != null) {
+                return false;
+            }
+
+            if (first.right != null && second.right != null) {
+                q1.add(first.right);
+                q2.add(second.right);
+            } else if (first.right != null || second.right != null) {
+                return false;
+            }
+
         }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return true;
     }
 }
