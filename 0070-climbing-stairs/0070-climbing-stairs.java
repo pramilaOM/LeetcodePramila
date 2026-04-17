1c666d1510ef61577b3488aacbd4048d4f724068
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-04-17 08:07:53 -0400

    Time: 0 ms (100%), Space: 41.9 MB (76.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,20 +2,20 @@ class Solution {
     int[] t = new int[46];
 
     public int climbStairs(int n) {
-        if (n == 1 || n == 2 || n == 3) {
-            t[n] = n;
-            return t[n];
-        }
-
         Arrays.fill(t, -1);
+        return solve(n);
+    }
 
-        if (t[n] != -1) {
+    int solve(int n) {
+        if (n == 1 || n == 2 || n == 3)
+            return n;
+
+        if (t[n] != -1)
             return t[n];
-        }
-        int stepOne = climbStairs(n - 1);
-        int stepTwo = climbStairs(n - 2);
+
+        int stepOne = solve(n - 1);
+        int stepTwo = solve(n - 2);
 
         return t[n] = stepOne + stepTwo;
-
     }
 }
