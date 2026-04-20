91128dd8e9789b373b20fff4ccbf1561e14cb036
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-04-20 09:29:21 -0400

    Time: 125 ms (5.87%), Space: 46.9 MB (5.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,11 @@
 class Solution {
     public boolean isValid(String s) {
-        //https://www.youtube.com/watch?v=3ssh_mk4LqY
-        Stack<Character> st = new Stack<>();
+        int prevLen;
+        do {
+            prevLen = s.length();
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        } while (s.length() != prevLen);
 
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                st.push(')');
-            } else if (ch == '{') {
-                st.push('}');
-            } else if (ch == '[') {
-                st.push(']');
-            } else if (st.isEmpty() || st.peek() != ch) {
-                return false;
-            } else {
-                st.pop();
-            }
-        }
-
-        return st.isEmpty();
+        return s.length() == 0;
     }
 }
