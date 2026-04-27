5bd97486d08c7e843381c5edc8851f8460790319
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-04-27 08:43:48 -0400

    Time: 0 ms (100%), Space: 43.8 MB (9.4%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,24 +1,18 @@
-import java.math.BigInteger;
-
 class Solution {
     public int[] plusOne(int[] digits) {
-        StringBuilder sb = new StringBuilder();
-        for (int d : digits) {
-            sb.append(d);
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0; // carry
         }
 
-        // Convert to BigInteger and add 1
-        BigInteger num = new BigInteger(sb.toString());
-        num = num.add(BigInteger.ONE);
+        int[] result = new int[n + 1];
+        result[0] = 1;
 
-        // Convert back to array
-        String result = num.toString();
-        int[] ans = new int[result.length()];
-
-        for (int i = 0; i < result.length(); i++) {
-            ans[i] = result.charAt(i) - '0';
-        }
-
-        return ans;
+        return result;
     }
 }
